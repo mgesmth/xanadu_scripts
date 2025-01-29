7f3ba7e72684eb59cff0c10a3ff1f1103c0769d9
Meg Smith <meg8130@student.ubc.ca>
2025-01-29 15:20:24 -0500

    added scaffold command

--- quast012.sh
+++ quast012.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=general
 #SBATCH --qos=general
 #SBATCH --mail-type=ALL
-#SBATCH --mem=80G
+#SBATCH --mem=150G
 #SBATCH --mail-user=meg8130@student.ubc.ca
 #SBATCH -o quast1_2.%j.out
 #SBATCH -e quast1_2.%j.err
@@ -13,9 +13,9 @@ module load quast/5.2.0
 
 indir=/core/projects/EBP/smith/scaffold
 outdir=/home/FCAM/msmith/quast_out/1_2
-quast=/isg/shared/apps/quast/5.2.0/quast.py
 
-python3 $quast -e $indir/intDF011_scaffolds_final.fasta -o $outdir
+python3 quast -t 12 --split-scaffolds --large \ 
+${indir}/intDF011_scaffolds_final.fasta -o $outdir
 
 
 
