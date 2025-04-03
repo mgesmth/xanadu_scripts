10464945ac5bdeca000f96097cc4f4a308b70bf9
Meg Smith <meg8130@student.ubc.ca>
2025-04-03 16:48:36 -0400

    syntax

--- minigraph_interior_himem2.sh
+++ minigraph_interior_himem2.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=himem2
 #SBATCH --qos=himem
 #SBATCH --cpus-per-task=24
-#SBATCH --mem=1000G
+#SBATCH --mem=750G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=meg8130@student.ubc.ca
 #SBATCH -o minigraph_interior.%j.out
@@ -13,9 +13,10 @@ echo `hostname`
 
 home=/home/FCAM/msmith
 core=/core/projects/EBP/smith
-prim=${core}/CBP_assemblyfiles/interior_primary_final.fa
-alt=${core}/CBP_assemblyfiles/interior_alternate_final.fa
-out=${home}/minigraph_out/interior_primalt
+prim_scaff1=${core}/CBP_assemblyfiles/interior_primary_scaffold1.fa
+alt=${core}/CBP_assemblyfiles/interior_alternate_1Mb.fa
+coa=${core}/coastal/coastalDF_scaffrenamed_sorted_1Mb.fa
+out=${home}/minigraph_out/interior_primalt_toscaff1
 
 #test - succeeded
 #test=${core}/bin/minigraph/test
@@ -39,5 +40,5 @@ export PATH="${core}/bin/gfatools-0.5:$PATH"
 #minigraph -cxggs -l 10k ${test_gfa} ${human} ${chimp} ${orang} > "${test_out}.gfa"
 #gfatools bubble "${test_out}.gfa" > "${test_out}.bed"
 
-minigraph -cxggs -t 24 -U 10,50 ${prim} ${alt} > "${out}.gfa"
+minigraph -cxggs -t 24 ${prim_scaff1} ${alt} ${coa} > "${out}.gfa"
 gfatools bubble "${out}.gfa" > "${out}.bed"
