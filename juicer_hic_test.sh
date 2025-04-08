1441923074cd6a4d8d3a0e61d3463286b4df6eda
Meg Smith <meg8130@student.ubc.ca>
2025-04-08 14:37:05 -0400

    testing the Juicer script using data from juicer git

---
+++ juicer_hic_test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=juicer
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=meg8130@student.ubc.ca
+#SBATCH -o juicer.%j.out
+#SBATCH -e juicer.%j.err
+
+echo `hostname`
+
+#Define variables ---
+juicedir=/scratch/msmith/juicetest
+reference=${juicedir}/references/Homo_sapiens_assembly19.fasta
+juicescripts=${juicedir}/scripts
+site="Arima"
+genomeid="test"
+threads="4"
+shortpart="general"
+longpart="himem2"
+
+#Executables ----
+module load bwa/0.7.17
+module load python/3.8.1
+module load juicer/1.8.9
+export PATH="/home/FCAM/msmith/scripts:$PATH"
+
+test_contact_maps.sh \
+    -d ${juicedir} \
+    -s ${site} \
+    -g ${genomeid} \
+    -z ${reference} \
+    -D ${juicescripts} \
+    -t ${threads} \
+    -q "${shortpart}" \
+    -l "${longpart}"
---
+++ make_contact_maps.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+if [[ ( $@ == "--help") ||  $@ == "-h" ]]
+then
+    echo "Usage: ./make_contact_maps.sh -d <TOPDIR> -s <SITE> -g <GENOMEID> -z <GENOMEPATH> -D <SCRIPTS> -t <THREADS>"
+    echo ""
+    echo "Build contact maps from unprocessed Hi-C data."
+    echo ""
+    echo "dependencies:"
+    echo ""
+    echo "    bwa/0.7.17"
+    echo "    python/3.8.1"
+    echo "    juicer/1.8.9"
+    echo ""
+    echo "Requires at least 4 cores and 64GB RAM."
+    echo ""
+    echo "positional arguments:"
+    echo ""
+    echo "-d <TOPDIR>      Top level directory. Must contain ./fastq, which contains Hi-C fastqs or is soft-linked to them and ./restriction_sites."
+    echo "-s <SITE>        Restriction enzyme used in library prep."
+    echo "-g <GENOMEID>    A unique identifier for your genome."
+    echo "-z <GENOMEPATH>  Path to the reference genome."
+    echo "-D <SCRIPTS>     Path to the juicer scripts."
+    echo "-t <THREADS>     Number of threads for BWA alignment."
+    echo ""
+    echo ""
+	exit 0
+fi
+
+OPTSTRING="t:d:s:g:p:y:z:D:"
+while getopts ${OPTSTRING} opt
+do
+    case ${opt} in
+	t)
+	 threads=${OPTARG};;
+	d)
+	 topdir=${OPTARG};;
+	s)
+	 site=${OPTARG};;
+	g)
+	 genome_id=${OPTARG};;
+	p)
+	 chromsizes=${OPTARG};;
+	z)
+	 ref=${OPTARG};;
+	D)
+	 scripts=${OPTARG};;
+        :)
+         echo "option ${OPTARG} requires an argument."
+         exit 1
+	;;
+        ?)
+         echo "invalid option: ${OPTARG}"
+         exit 1
+	;;
+    esac
+done
+
+
+if [ ! -f "${ref}.bwt" ]; then
+    echo "BWA index not found. Beginning indexing..."
+    bwa index ${ref}
+else
+    echo "BWA index found."
+fi
+
+#Generate restriction site files -----
+if [ ! -f "${topdir}/restriction_sites/${genome_id}_${site}.txt" ]; then
+    echo "Restriction site file not found. Running generate_site_positions.py...."
+    cd ${topdir}/restriction_sites
+    python ${scripts}/generate_site_positions.py "${site}" "${genome_id}" "${ref}"
+    sitefile="${topdir}/restriction_sites/${genome_id}_${site}.txt"
+else
+    echo "Restriction site file found. Moving on..."
+    sitefile="${topdir}/restriction_sites/${genome_id}_${site}.txt"
+fi
+
+#Chromsizes file ----
+cd ${topdir}/references
+if [ -f "${sitefile}" ]; then
+    awk 'BEGIN{OFS="\t"}{print $1, $NF}' "${sitefile}" > "${genome_id}.chrom.sizes"
+    chromsizes="${topdir}/references/${genome_id}.chrom.sizes"
+else
+    echo "Error: Restriction site file ${sitefile} not found."
+    exit 1
+fi
+
+#Run Juicer -----
+cd ${topdir}
+
+${scripts}/juicer.sh \
+-g "${genome_id}" \
+-d "${topdir}" \
+-s "${site}" \
+-p "${chromsizes}" \
+-y "${sitefile}" \
+-z "${ref}" \
+-t "${threads}"	
+
+
