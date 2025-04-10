09de657a8ba1d2a7dbaa93c7d54600759733b01f
Meg Smith <meg8130@student.ubc.ca>
2025-04-10 17:53:33 -0400

    trying new options to bend it to my will

--- minigraph_interior.sh
+++ minigraph_interior.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=minigraph
-#SBATCH --partition=himem
+#SBATCH --partition=himem2
 #SBATCH --qos=himem
 #SBATCH --cpus-per-task=24
-#SBATCH --mem=500G
+#SBATCH --mem=1000G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=meg8130@student.ubc.ca
 #SBATCH -o minigraph_interior.%j.out
@@ -13,9 +13,9 @@ echo `hostname`
 
 home=/home/FCAM/msmith
 core=/core/projects/EBP/smith
-prim=${core}/CBP_assemblyfiles/interior_primary_final.fa
-alt=${core}/CBP_assemblyfiles/interior_alternate_final.fa
-out=${home}/minigraph_out/interior_primalt
+prim=${core}/CBP_assemblyfiles/interior_primary_1Mb.fa
+alt=${core}/CBP_assemblyfiles/interior_alternate_1Mb.fa
+out=${home}/minigraph_out/interior_primalt_newopts
 
 #test - succeeded
 #test=${core}/bin/minigraph/test
@@ -26,9 +26,9 @@ out=${home}/minigraph_out/interior_primalt
 #test_out=${test}/test
 
 #EXEC
-module load zlib/1.2.11
+export PATH="${core}/bin/zlib-1.3.1:$PATH"
 export PATH="${core}/bin/minigraph-0.21:$PATH"
-#export PATH="${core}/bin/gfatools:$PATH"
+export PATH="${core}/bin/gfatools-0.5:$PATH"
 
 #test real variables - succeeded
 #ls ${prim}
@@ -36,8 +36,8 @@ export PATH="${core}/bin/minigraph-0.21:$PATH"
 #echo ${out}
 
 #test command
-minigraph -cxggs -l 10k ${test_gfa} ${human} ${chimp} ${orang} > "${test_out}.gfa"
-gfatools bubble "${test_out}.gfa" > "${test_out}.bed"
+#minigraph -cxggs -l 10k ${test_gfa} ${human} ${chimp} ${orang} > "${test_out}.gfa"
+#gfatools bubble "${test_out}.gfa" > "${test_out}.bed"
 
-minigraph -cxggs -t 24 -U 10,50 ${prim} ${alt} > "${out}.gfa"
+minigraph -cxggs -t 24 -U10,50 -k21 --min-cov-mapq 10 -q 10 -l 75k --gg-match-pen 10 ${prim} ${alt} > "${out}.gfa"
 gfatools bubble "${out}.gfa" > "${out}.bed"
