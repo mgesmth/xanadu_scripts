68b70ccdbc4c88d200cac5aae3c799bfd9a532ed
Meg Smith <meg8130@student.ubc.ca>
2025-04-14 14:46:15 -0400

    tried one more time. failed.

--- minigraph_interior.sh
+++ minigraph_interior.sh
@@ -27,8 +27,8 @@ out=${home}/minigraph_out/interior_primalt_newopts
 
 #EXEC
 export PATH="${core}/bin/zlib-1.3.1:$PATH"
-export PATH="${core}/bin/minigraph-0.21:$PATH"
-export PATH="${core}/bin/gfatools-0.5:$PATH"
+export PATH="${core}/bin/minigraph:$PATH"
+export PATH="${core}/bin/gfatools:$PATH"
 
 #test real variables - succeeded
 #ls ${prim}
@@ -39,5 +39,5 @@ export PATH="${core}/bin/gfatools-0.5:$PATH"
 #minigraph -cxggs -l 10k ${test_gfa} ${human} ${chimp} ${orang} > "${test_out}.gfa"
 #gfatools bubble "${test_out}.gfa" > "${test_out}.bed"
 
-minigraph -cxggs -t 24 -U10,50 -k21 --min-cov-mapq 10 -q 10 -l 75k --gg-match-pen 10 ${prim} ${alt} > "${out}.gfa"
+minigraph -cxggs -t 24 -k21 -U10,50 --min-cov-mapq 20 -q 20 --gg-match-pen 10 ${prim} ${alt} > "${out}.gfa"
 gfatools bubble "${out}.gfa" > "${out}.bed"
