13509f948d89181d92eb59a5b3dc01874edfb2c3
Meg Smith <meg8130@student.ubc.ca>
2025-04-23 17:02:53 -0400

    running this with rest data rather than the whole thing to make sure it works

--- sbatch_juicertools_gsp.sh
+++ sbatch_juicertools_gsp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -p himem
-#SBATCH -q himem
-#SBATCH -c 24
-#SBATCH --mem=250G
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 8
+#SBATCH --mem=80G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=meg8130@student.ubc.ca
-#SBATCH -o juicer_sites.%j.out
-#SBATCH -e juicer_sites.%j.err
+#SBATCH -o juicer_sites_test.%j.out
+#SBATCH -e juicer_sites_test.%j.err
 
 echo `hostname`
 
@@ -16,11 +16,11 @@ module load juicer/1.22.01
 module load pairtools/0.2.2
 export PATH="/home/FCAM/msmith/scripts:$PATH"
 topdir=/core/projects/EBP/smith/juicer_primary
-pairs=/scratch/msmith/interior_primary_hiCaln_nodups.pairs
+pairs=${topdir}/test_juicertools.pairs
 site="Arima"
-gid="interior_primary"
-gpath=${topdir}/references/interior_primary_final.fa
-output="${topdir}/interior_primary_contacts"
+gid="interior_primary_test"
+gpath=${topdir}/references/test_juicertools.fa
+output="${topdir}/test_juicertools"
 
 make_contact_maps_generate_site_positions.sh \
 	-t 24 \
