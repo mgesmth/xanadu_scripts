57cc213238d9d1332711e7ebdb99433710330545
mgesmth <meg8130@student.ubc.ca>
2025-05-02 11:53:41 -0700

    Update and rename SVbed2vcf.sh to minigraph_callpath.sh

---
+++ minigraph_callpath.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -J minigraph_call
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 24
+#SBATCH --mem=150G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=meg8130@student.ubc.ca
+#SBATCH -o %x.%j.out
+#SBATCH -e %x.%j.err
+
+echo `hostname`
+
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+
+module load zlib/1.2.11
+export PATH="${core}/bin/minigraph-0.21:$PATH"
+export PATH="${core}/bin/gfatools:$PATH"
+
+minidir=/home/FCAM/msmith/minigraph_out
+gfa=${minidir}/all_brokenscaffolds.gfa
+coastal=${scratch}/coastal_1Mb_broken.fa
+
+minigraph -cxasm --call -t24 "$gfa" "$coastal" > "${minidir}/coastal_path.bed"
