04558de214dd9c199b414fbcde40ca9d56e78b5c
mgesmth <meg8130@student.ubc.ca>
2025-05-09 14:11:06 -0700

    Update arrowhead.sh

--- arrowhead.sh
+++ arrowhead.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 #SBATCH -J juicer_arrowhead
-#SBATCH -p general
-#SBATCH -q general
+#SBATCH -p himem2
+#SBATCH -q himem
 #SBATCH -c 12
-#SBATCH --mem=250G
+#SBATCH --mem=300G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=meg8130@student.ubc.ca
 #SBATCH -o %x.%j.out
 #SBATCH -e %x.%j.err
+
+module load python/3.10.1
+module load bwa/0.7.17
+module load juicer/1.22.01
+
+scratch=/scratch/msmith
+core=/core/projects/EBP/smith
+hic=${core}/juicer_primary/interior_primary_contacts.hic
+out=${scratch}/interior_primary_contactdomains.txt
+
+arrowhead --threads 12 -k KR "${hic}" "${out}"
