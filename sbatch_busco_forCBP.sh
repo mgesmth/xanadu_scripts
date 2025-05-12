edda9f69ae9d51ad2e6c01c3c79d7dfd4478c0c7
mgesmth <meg8130@student.ubc.ca>
2025-05-12 13:54:24 -0700

    Update sbatch_busco_forCBP.sh

--- sbatch_busco_forCBP.sh
+++ sbatch_busco_forCBP.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #SBATCH -J busco
-#SBATCH -p himem2
-#SBATCH -q himem
-#SBATCH -c 28
-#SBATCH --mem=500G
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 12
+#SBATCH --mem=200G
+#SBATCH --array=[0-2]%3
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=meg8130@student.ubc.ca
 #SBATCH -o %x.%j.out
 #SBATCH -e %x.%j.err
 
-echo `hostname`
+echo "[M]: Hostname: `hostname`"
 
 source /home/FCAM/msmith/busco/.venv/bin/activate
 module load blast/2.7.1 augustus/3.6.0 hmmer/3.3.2 R/4.2.2 java/17.0.2 bbmap/39.08 prodigal/2.6.3
@@ -22,14 +25,20 @@ export PATH="/core/projects/EBP/smith/bin/miniprot:$PATH"
 
 home=/home/FCAM/msmith
 core=/core/projects/EBP/smith
-prim=${core}/CBP_assemblyfiles/interior_primary_final.fa
-alt=${core}/CBP_assemblyfiles/interior_alternate_final.fa
-mine=${core}/scaffold/withpairtools_noerrorcorrect/intDF011_scaffolds_final.fa
-coastal=${core}/coastal/coastalDF_scaffrenamed_sorted.fa
-mode="genome"
-db1="${home}/busco/busco_downloads/lineages/viridiplantae_odb12"
-db2="eukaryota_odb12"
-out1=${home}/busco/intDF011_viridiplantae
-out2=${home}/busco/intDF011_eukaryota
+dbs=($(echo -e "eukaryota_odb12\nembryophyta_odb12\nviridiplantae_odb12"))
 
 /home/FCAM/msmith/scripts/run_busco.sh -t 28 -i "${mine}" -m "${mode}" -l "${db1}" -o "${out1}"
+
+#names for db for this task:
+db="${dbs[$SLURM_ARRAY_TASK_ID]}"
+prim=${core}/CBP_assemblyfiles/interior_primary_final.fa
+alt=${core}/CBP_assemblyfiles/interior_alternate_final.fa
+coastal=${core}/coastal/coastalDF_scaffrenamed_sorted.fa
+mode="genome"
+out1="${home}/busco/prim"
+out2="${home}/busco/alt"
+out3="${home}/busco/coa"
+
+/home/FCAM/msmith/scripts/run_busco.sh -t 12 -i "${prim}" -m "${mode}" -l "${db}" -o "${out1}"
+/home/FCAM/msmith/scripts/run_busco.sh -t 12 -i "${alt}" -m "${mode}" -l "${db}" -o "${out2}"
+/home/FCAM/msmith/scripts/run_busco.sh -t 12 -i "${coa}" -m "${mode}" -l "${db}" -o "${out3}"
