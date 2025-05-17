9bccaae1d3c3f7d192778ab9f2e24868c5b569bf
Meg Smith <meg8130@student.ubc.ca>
2025-05-17 02:07:50 -0400

    commit

--- run_minigraph-cactus_slurm.sh
+++ run_minigraph-cactus_slurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J mg-cactus-submit
-#SBATCH -p gpu
+#SBATCH -p general
 #SBATCH -q general
-#SBATCH -c 64
-#SBATCH --mem=500G
+#SBATCH -c 1
+#SBATCH --mem=2G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=meg8130@student.ubc.ca
 #SBATCH -o %x.%j.out
@@ -23,7 +23,8 @@ seqfile=${core}/mg_cactus/mg-cactus_fas.txt
 sarg="-q general"
 
 singularity exec /isg/shared/mantis/apps/cactus/2.9.8/cactus_v2.9.8-gpu.sif \
-cactus-pangenome "${scratch}/mg_tmp" "$seqfile" --outDir ${outdir} --outName ${outpref} \
-  --reference interiorprim --refContigs $(for i in $(seq 1 13) ; do printf "chr$i" ; done) --otherContig scaffoldOther \
-  --consMemory 500G --indexMemory 300G --mgMemory 500G --maxDisk 400G \
+cactus-pangenome "$scratch" "$seqfile" --outDir ${outdir} --outName ${outpref} \
+  --reference interiorprim --refContigs $(for i in $(seq 1 13) ; do printf "chr$i" ; done) --otherContig chrOther \
+  --batchSystem slurm  --slurmPartition gpu --slurmGPUPartition gpu --slurmArgs "$sarg" \
+  --batchLogsDir ${outdir}/log --workDir ${scratch} --maxMemory 500G --consCores 64 --doubleMem true \
   --vcf --viz --gfa --gbz --odgi --chrom-og
