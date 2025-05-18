1a78aef3a5dce9b6c1d365f377e169e98244efd9
Meg Smith <meg8130@student.ubc.ca>
2025-05-18 14:25:00 -0400

    trying to get stupid minigraph-cactus to work. I got both SLURM and single machine versions to run, but SM seems to not be fed the right amount of disk and I'm not sure Mantis has enough RAM for minigraph.

---
+++ run_minigraph-cactus_himem.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -J mg-cactus-submit
+#SBATCH -p himem
+#SBATCH -q himem
+#SBATCH -c 64
+#SBATCH --mem=750G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=meg8130@student.ubc.ca
+#SBATCH -o %x.%j.out
+#SBATCH -e %x.%j.err
+
+echo "[M]: Host Name: `hostname`"
+
+module load singularity/3.10.0
+module load squashfs/4.3
+
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+outpref=dougfir_mgcactus
+outdir=/core/projects/EBP/smith/mg_cactus
+seqfile=${core}/mg_cactus/mg-cactus_fas.txt
+sarg="-q general"
+
+singularity exec /isg/shared/mantis/apps/cactus/2.9.8/cactus_v2.9.8-gpu.sif \
+cactus-pangenome "${scratch}/mg_tmp" "$seqfile" --outDir ${outdir} --outName ${outpref} \
+  --reference interiorprim --refContigs $(for i in $(seq 1 13) ; do printf "chr$i" ; done) --otherContig scaffoldOther \
+  --consMemory 500G --indexMemory 300G --mgMemory 500G --maxDisk 1000G \
+  --vcf --viz --gfa --gbz --odgi --chrom-og
--- run_minigraph-cactus_slurm.sh
+++ run_minigraph-cactus_slurm.sh
@@ -11,20 +11,19 @@
 
 echo "[M]: Host Name: `hostname`"
 
-module load singularity/3.10.0
-module load squashfs/4.3
-
 home=/home/FCAM/msmith
 core=/core/projects/EBP/smith
 scratch=/scratch/msmith
 outpref=dougfir_mgcactus
 outdir=/core/projects/EBP/smith/mg_cactus
 seqfile=${core}/mg_cactus/mg-cactus_fas.txt
-sarg="-q general"
+sarg="-q himem"
 
-singularity exec /isg/shared/mantis/apps/cactus/2.9.8/cactus_v2.9.8-gpu.sif \
-cactus-pangenome "$scratch" "$seqfile" --outDir ${outdir} --outName ${outpref} \
+source ${core}/bin/cactus/cactus_env/bin/activate
+
+cactus-pangenome "${scratch}/mg_tmp" "$seqfile" --outDir ${outdir} --outName ${outpref} \
   --reference interiorprim --refContigs $(for i in $(seq 1 13) ; do printf "chr$i" ; done) --otherContig chrOther \
-  --batchSystem slurm  --slurmPartition gpu --slurmGPUPartition gpu --slurmArgs "$sarg" \
-  --batchLogsDir ${outdir}/log --workDir ${scratch} --maxMemory 500G --consCores 64 --doubleMem true \
-  --vcf --viz --gfa --gbz --odgi --chrom-og
+  --batchSystem slurm  --slurmPartition himem --slurmGPUPartition gpu --slurmArgs "$sarg" \
+  --batchLogsDir ${outdir}/log --workDir ${scratch} --maxMemory 750G --consCores 64 \
+  --indexCores 36 --doubleMem true --mgCores 36 --mapCores 8 \
+  --vcf --viz --gfa --gbz --odgi --chrom-og --binariesMode singularity
