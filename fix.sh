fcba6eec26b257d8e7b83547d4b4779a867f40b3
mgesmth <meg8130@student.ubc.ca>
2025-05-24 13:57:39 -0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -23,5 +23,16 @@ done
 cut -f2 interior_primary_1Mb.fa.fai | paste - scafflengths_check.txt | less #they match!
 
 #Create the index
-#new=(0)
+for scaffold in $(cat scaffolds.txt) ; do
+  new=(0)
+  contig_lengths=(cut -f2 contig_lengths_adjusted_${scaffold}.tsv)
+  numlines=$(wc -l contig_lengths_adjusted_${scaffold}.tsv | cut -d ' ' -f1)
+  numlines_minus1=$(echo $((${numlines}-1)))
+  for i in $(seq 1 ${numlines_minus1}) ; do
+    h=$(echo $((${i}-1)))
+    new[$i]=$(echo $((${contig_lengths[$h]}+${new[$h]})))
+  done
+  printf '%s\n' "${new[@]}" > foo.txt
+  cut -f1 contig_lengths_adjusted_${scaffold}.tsv | paste - foo.txt > ${scaffold}.idx && rm foo.txt
+done
 
