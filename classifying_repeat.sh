94cd3d96b1b82cc983b42908ae5cf42347ef45e1
mgesmth <meg8130@student.ubc.ca>
2025-06-04 15:33:09 -0700

    Update classifying_repeat.sh

--- classifying_repeat.sh
+++ classifying_repeat.sh
@@ -2,22 +2,28 @@
 #SBATCH -J getseq
 #SBATCH -p general
 #SBATCH -q general
-#SBATCH -c 12
-#SBATCH --mem=36G
-#SBATCH -o %x.%j.out
-#SBATCH -e %x.%j.err
+#SBATCH -n 1
+#SBATCH -c 4
+#SBATCH --mem=10G
+#SBATCH --array=0-630%100
+#SBATCH -o %x.%a.%A.out
+#SBATCH -e %x.%a.%A.err
 
 echo `hostname`
 
 export PATH="/core/projects/EBP/smith/bin/gfatools:$PATH"
 
-indir=/home/FCAM/msmith/svs/minigraph_out
+indir=/home/FCAM/msmith/svs/minigraph_out/split
 outdir=/scratch/msmith/classifying_repeat
 cd ${outdir}
-cat ${indir}/all_brokenscaffolds_verified.bed | while read -r line ; do
+
+chunks=($(ls -1 ${indir}/chunk_*))
+file=${chunks[$SLURM_ARRAY_TASK_ID]}
+
+cat ${file} | while read -r line ; do
   recname=$(echo "$line" | awk '{print $1":"$2"-"$3}')
   segs=$(echo "$line" | cut -f12)
-  gfatools view -l "$segs" ${indir}/all_brokenscaffolds.gfa > tmp.gfa
+  gfatools view -l "$segs" ${indir}/all_brokenscaffolds.gfa > "$recname".gfa
   gfatools gfa2fa tmp.gfa > "$recname".fa
-  rm tmp.gfa
+  rm "$recname".gfa
 done
