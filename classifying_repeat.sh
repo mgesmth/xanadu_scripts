3ed02d6b86086f6c457236e284a0eaa36fa3d06d
mgesmth <meg8130@student.ubc.ca>
2025-06-04 15:51:58 -0700

    Update classifying_repeat.sh

--- classifying_repeat.sh
+++ classifying_repeat.sh
@@ -13,7 +13,7 @@ echo `hostname`
 
 export PATH="/core/projects/EBP/smith/bin/gfatools:$PATH"
 
-gfa=/home/FCAM/msmith/minigraph_out/all_brokenscaffolds.gfa
+gfa=/home/FCAM/msmith/svs/minigraph_out/all_brokenscaffolds.gfa
 indir=/home/FCAM/msmith/svs/minigraph_out/split
 outdir=/scratch/msmith/classifying_repeat
 cd ${outdir}
