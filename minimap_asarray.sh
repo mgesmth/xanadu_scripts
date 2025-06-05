104df30d8ecfb99fbfd33241682699ffadaddce1
mgesmth <meg8130@student.ubc.ca>
2025-06-05 12:09:44 -0700

    Create minimap_asarray.sh

---
+++ minimap_asarray.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -J minimap_arr
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -n 1
+#SBATCH -c 6
+#SBATCH --mem=20G
+#SBATCH --array=0-x%15
+#SBATCH -o %j.%A.%a.out
+#SBATCH -e %j.%A.%a.err
+
+echo "[M]: Hostname: `hostname`"
+
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+hifidir=${scratch}/hifi_split
+prim=${core}/CBP_assemblyfiles/interior_primary_final.fa
+out=${scratch}/hifi_out
+
+module load samtools/1.19.2
+module load minimap2/2.28
+
+FILES=($(ls -1 ${hifidir}/hifisplit_*))
+FQ=${FILES[$SLURM_ARRAY_TASK_ID]}
+base=$(basename ${FQ})
+
+echo "[M]: Welcome to minimap alignment task ${SLURM_ARRAY_TASK_ID}."
+
+${home}/scripts/minimap2_hifi.sh -t 4 -r "$prim" -q "$FQ" -o "${out}_${SLURM_ARRAY_TASK_ID}.bam"
