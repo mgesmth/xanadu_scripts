7b7c89994326682a3cf928ea4159e4cfbd554b46
Meg Smith <meg8130@student.ubc.ca>
2025-07-15 15:28:39 -0700

    Create split_hic_fastqs.sh

---
+++ manual_curation/split_hic_fastqs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH -J split_hic_fastqs
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 24
+#SBATCH --mem=500G
+#SBATCH -o %x.%j.out
+#SBATCH -e %x.%j.err
+
+set -e
+
+date
+echo "[M]: Host Name: `hostname`"
+
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+R1=${home}/hiC_data/allhiC_R1.fastq.gz
+R2=${home}/hiC_data/allhiC_R2.fastq.gz
+outdir=${scratch}/hic_split
+if [[ ! -d "$outdir" ]] ; then
+  mkdir ${outdir}
+fi
+splitN=300
+
+module load seqkit/2.10.0
+
+echo "[M]: Splitting ${R1} and ${R2} into ${splitN} parts..."
+
+seqkit split2 -1 "$R1" -2 "$R2" -p "$splitN" -O "$outdir" -f
+if [[ $? -eq 0 ]] ; then
+  echo "[M]: Splitting complete."
+  date
+  exit 0
+else
+  echo "[M]: Splitting failed. Exit code $?"
+  date
+  exit 1
+fi
