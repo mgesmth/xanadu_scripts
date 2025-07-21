3233ffec64801ae6020d102586bc8c27cde0a3a0
Meg Smith <meg8130@student.ubc.ca>
2025-07-21 15:33:26 -0700

    Create categorize_svs.py

---
+++ categorize_svs.py
@@ -0,0 +1,9 @@
+#!/bin/env python
+
+import os
+os.chdir(~/svs/minigraph/finalpangenome)
+
+with open("sv_allele_summary.csv") as f:
+  #return this first line as is - header line
+  f.readline()
+  
