c8db0a45740a9853d8dcdf2a1a7288b03259932b
Meg Smith <meg8130@student.ubc.ca>
2025-07-23 17:35:14 -0700

    Update adjust_coordinates_new.py

--- adjust_coordinates_new.py
+++ adjust_coordinates_new.py
@@ -1,29 +1,45 @@
 #!/bin/env python
 
-import pandas as pd
+print("[M]: Beginning creation of index dictionary.")
+with open("interior_primary_final.fa.fai") as f:
+  previous_line = None
+  length_dict={}
+  for line in f:
+    frag_1="primary_1"
+    frag_2="primary_2"
+    fields=line.strip().split('\t')
+    ori_scaffold=str(fields[0])
+    scaff_num=int(ori_scaffold.split('_')[1])
+    
+    #handle split scaffolds
+    if scaff_num < 8:
+      if frag_1 in ori_scaffold:
+        previous_line=line #update previous line for next iteration (for fragment 2)
+        length_dict.update({ori_scaffold: 0})
+      else if frag_2 in ori_scaffold:
+        previous_len=int(previous_line.strip().split('\t')[1])
+        add=previous_len+200
+        length_dict.update({ori_scaffold: add})
+      else:
+        raise Exception("[E]: previous line not stored correctly.")
 
-fai=open('interior_primary_final.fa.fai')
-ori_index=fai.readlines()
+    #handle unsplit scaffolds
+    elif scaff_num > 8:
+      length_dict.update({ori_scaffold: 0})
 
-length_dict={}
-#handle broken scaffolds
-for i in 0:13:
-  columns=ori_index[i].strip().split('\t')
-  name=columns[0]
-  frag_num=columns[0].split('_')[3]
-  if frag_num = 1:
-    length_dict.update({name: 0})
-    #First fragment comes first; create the add number when processing that fragment, 
-    #apply it when processing the second fragment
-    add=columns[1]+200 #add 200 for the gap
-  else if frag_num = 2:
-    length_dict.update({name: add})
+    else:
+      raise Exception("[E]: scaffold number not parsed correctly.")
+f.close()
 
-#handle unbroken scaffolds
-for i in 14:3011
-  columns=ori_index[i].strip().split('\t')
-  name=columns[0]
-  length_dict.update({name: 0})
+#Now we have the dictionary: let's update the coordinates in sv_allele_summary.tsv
+with open("sv_allele_summary.tsv", "r") as f, open("sv_allele_summary_updatedcoord.tsv", "w") as of:
+  header=f.readline()
+  of.write(header)
+  for line in f:
+    fields=line.strip().split('\t')
+    
+
+
 
   
   
