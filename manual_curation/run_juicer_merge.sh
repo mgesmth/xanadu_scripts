087da8aaf5bf90609b102b02c5faaf92e50871c0
Meg Smith <meg8130@student.ubc.ca>
2025-07-26 11:36:15 -0700

    Create run_juicer_merge.sh

---
+++ manual_curation/run_juicer_merge.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH -J run_juicer_merge
+#SBATCH -p himem2
+#SBATCH -q himem
+#SBATCH -c 6
+#SBATCH --mem=1000G
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=meg8130@student.ubc.ca
+#SBATCH -o %x.%j.out
+#SBATCH -e %x.%j.err
+
+set -e
+
+echo "[M]: Host Name: `hostname`"
+
+module load samtools/1.20
+module load bwa/0.7.17
+module load java-sdk/1.8.0_92
+
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+gid="intdf137"
+site="Arima"
+threads=36
+jd=${scratch}/juicer_formanualcur
+
+cd ${jd}/work/intdf137
+rm *_in.bam
+
+echo "[M]: Beginning juicer run."
+
+cd ${jd}
+#Okay - now run juicer (CPU version, modified for better handling of large files)
+scripts/juicer_justmerge.sh -f --assembly -g "$gid" -d "${jd}/work/intdf137" -s "$site" -S merge \
+-p references/intdf137.chrom.sizes -y restriction_sites/intdf137_Arima.txt \
+-z references/interior_primary_final.fa -D "$jd" -t "$threads"
+if [[ $? -e 0 ]] ; then
+  echo "[M]: Juicer complete! Bye."
+  date
+  exit 0
+else
+  echo "[E]: Juicer failed. Exit code $?"
+  date
+  exit 1
+fi
