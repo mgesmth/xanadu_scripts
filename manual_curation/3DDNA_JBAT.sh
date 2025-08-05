1d773f91f9a8b146248c81df0e1535e3461dfea0
Meg Smith <meg8130@student.ubc.ca>
2025-08-05 16:40:23 -0400

    commit

--- manual_curation/3DDNA_JBAT.sh
+++ manual_curation/3DDNA_JBAT.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -p general
-#SBATCH -q general
-#SBATCH -c 24
-#SBATCH --mem=500G
+#SBATCH -p himem2
+#SBATCH -q himem
+#SBATCH -c 36
+#SBATCH --mem=1200G
 #SBATCH -J 3DDNA_JBAT
 #SBATCH -o %x.%j.out
 #SBATCH -e %x.%j.err
@@ -10,6 +10,7 @@
 set -e
 date
 echo "[M]: Host Name: `hostname`"
+module load gnu-parallel/20160622
 
 home=/home/FCAM/msmith
 core=/core/projects/EBP/smith
@@ -18,9 +19,11 @@ prim=${core}/CBP_assemblyfiles/interior_primary_final.fa
 asm_file="${prim%.*}.assembly"
 merge_nodups=${scratch}/juicer_formanualcur/work/intdf137/aligned/merged_nodups.txt
 
-export PATH="${core}/bin/3d_dna:$PATH"
+export PATH="${core}/bin/3d-dna:$PATH"
+export TMPDIR=/scratch/msmith
 
-awk –f ${core}/bin/3d_dna/utils/generate-assembly-file-from-fasta.awk "$prim" > "$asm_file"
+#awk -f ${core}/bin/3d-dna/utils/generate-assembly-file-from-fasta.awk "$prim" > "$asm_file"
+
 #no output file specified, so run in scratch to control where output
 cd ${scratch}
-${core}/bin/3d_dna/visualize/run-assembly-visualizer.sh "$asm_file" "$merge_nodups"
+${core}/bin/3d-dna/visualize/run-assembly-visualizer.sh "$asm_file" "$merge_nodups"
--- phase_myasm/alnhic_hap1.sh
+++ phase_myasm/alnhic_hap1.sh
@@ -33,7 +33,7 @@ out=$(echo "$name" | sed 's/fastq.gz/bam/')
 
 echo "[M]: Welcome to task $SLURM_ARRAY_TASK_ID. We are aligning ${R1} and ${R2} to ${base}."
 
-bwa mem -SP5M -t 4 -R "$hap1_ctg" "$R1" "$R2" | \
+bwa mem -SP5M -t 4 "$hap1_ctg" "$R1" "$R2" | \
 samtools sort -@ 4 -m 2500M -O "bam" -o "${hic_splitout}/${out}"
 
 if [[ $? -eq 0 ]] ; then
