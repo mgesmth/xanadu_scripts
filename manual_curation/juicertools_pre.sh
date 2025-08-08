794ec324e7e10b2c7088f19d2909079c29771dbf
Meg Smith <meg8130@student.ubc.ca>
2025-08-08 17:58:58 -0400

    commit

--- manual_curation/juicertools_pre.sh
+++ manual_curation/juicertools_pre.sh
@@ -3,7 +3,7 @@
 #SBATCH -p himem2
 #SBATCH -q himem
 #SBATCH -c 36
-#SBATCH --mem=1200G
+#SBATCH --mem=1000G
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=meg8130@student.ubc.ca
 #SBATCH -o %x.%j.out
@@ -11,7 +11,7 @@
 
 date
 echo "[M]: Host Name: `hostname`"
-module load java
+module load java/17.0.2
 module load python/3.8.1
 
 home=/home/FCAM/msmith
@@ -22,10 +22,13 @@ topdir=${scratch}/juicer_formanualcur
 JUICER=${topdir}/scripts/common/juicer_tools.jar
 threads="36"
 tmpdir=${scratch}
-output="${scratch}/interior_primary_final.hic"
+output="${core}/interior_primary_final.hic"
 chromsizes=${core}/CBP_assemblyfiles/interior_primary_final.chrom.sizes
-contacts=${topdir}/work/intdf137/aligned/merged_dedup.bam
+contacts=${topdir}/work/intdf137/aligned/merged_nodups.txt
+res_sites=/scratch/msmith/juicer_formanualcur/restriction_sites/intdf137_Arima.txt
 
-java -XX:+UseParallelGC -Xms250G -Xmx1200G -jar $JUICER pre -v --threads "${threads}" \
--t "${tmpdir}" -f ${topdir}/restriction_sites/intdf137_Arima.txt \
-"${contacts}" "${output}" "${chromsizes}"
+mv ${topdir}/work/intdf137/aligned/merged_dedup.bam /core/projects/EBP/smith
+
+java -XX:+UseParallelGC -Xms250G -Xmx1000G -jar $JUICER pre -f "$res_sites" -v -t "${tmpdir}" "${contacts}" "${output}" /core/projects/EBP/smith/CBP_assemblyfiles/interior_primary_final.chrom.sizes
+
+
