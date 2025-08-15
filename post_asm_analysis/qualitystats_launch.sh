78755a2189d41453bbf445c0dd5fcad55e6f27c2
Meg Smith <meg8130@student.ubc.ca>
2025-08-15 09:17:22 -0700

    Update and rename assess_and_preprocess.sh to qualitystats_launch.sh

---
+++ post_asm_analysis/qualitystats_launch.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+#SBATCH -J postmancur_launch
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 1
+#SBATCH --mem=2G
+#SBATCH -o %x.%j.out
+#SBATCH -e %x.%j.err
+
+set -e
+set -a
+
+echo "[M]: Host Name: `hostname`"
+
+#variables
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+outdir=${core}/manual_curation_files
+prim=${outdir}/interior_primary_final_mancur.fa
+baseprim=$(basename ${prim})
+alt=${core}/CBP_assemblyfiles/interior_alternate_final.fa
+
+export PATH="${home}/scripts/post_asm_analysis:$PATH"
+
+if [[ ! -d ${core}/manual_curation_files/log ]] ; then
+  mkdir ${core}/manual_curation_files/log
+  log=${core}/manual_curation_files/log
+fi
+
+#Quast ----
+jid_quast=`sbatch <<- HEADER | egrep -o -e "\b[0-9]+$"
+#!/bin/bash
+#SBATCH -J quast
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 12
+#SBATCH --mem=150G
+#SBATCH -o ${log}/%x.%j.out
+#SBATCH -e ${log}/%x.%j.err
+	
+date
+echo "[M]: Host Name: `hostname`"
+echo "[M]: Beginning QUAST analysis of ${baseprim}"
+module load quast/5.2.0
+quast=/isg/shared/apps/quast/5.2.0/quast.py
+threads="$(getconf _NPROCESSORS_ONLN)"
+outquast=${outdir}/quast
+if [[ ! -d ${outquast} ]] ; then
+  mkdir ${outquast}
+fi
+
+python3 $quast -t ${threads} --split-scaffolds --large -o ${outquast} ${prim}
+if [[ $? -eq 0 ]] ; then
+echo "[M]: Done."
+exit 0
+else
+echo "[E]: QUAST run failed. Exit code $?"
+fi
+HEADER`
+
+#BUSCO ----
+##Eukaryota ----
+jid_busco_euk=`sbatch <<- HEADER | egrep -o -e "\b[0-9]+$"
+#!/bin/bash
+#SBATCH -J busco_euk
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 12
+#SBATCH --mem=200G
+#SBATCH -o ${log}/%x.%j.out
+#SBATCH -e ${log}/%x.%j.err
+
+database="eukaryota_odb12"
+date
+echo "[M]: Host Name: `hostname`"
+echo "[M]: Beginning BUSCO analysis of ${baseprim} against database ${database}"
+#Module files
+source /home/FCAM/msmith/busco/.venv/bin/activate
+module load blast/2.7.1 augustus/3.6.0 hmmer/3.3.2 R/4.2.2 java/17.0.2 bbmap/39.08 prodigal/2.6.3
+export AUGUTUS_CONFIG_PATH="/core/projects/EBP/smith/busco/config"
+threads="$(getconf _NPROCESSORS_ONLN)"
+export PATH="/home/FCAM/msmith/R/x86_64-pc-linux-gnu-library/4.2:$PATH"
+export PATH="/core/projects/EBP/smith/bin/miniprot:$PATH"
+outbusco=${outdir}/busco
+if [[ ! -d ${outbusco} ]] ; then
+  mkdir ${outbusco}
+fi
+
+busco --offline -c ${threads} -i ${prim} -m "genome" -f -l ${database} -o "prim_mancur_${database}" --out_path ${outbusco}
+if [[ $? -eq 0 ]] ; then
+echo "[M]: Done."
+exit 0
+else
+echo "[E]: BUSCO run failed. Exit code $?"
+fi
+HEADER`
+
+##Viridiplantae----
+jid_busco_virid=`sbatch <<- HEADER | egrep -o -e "\b[0-9]+$"
+#!/bin/bash
+#SBATCH -J busco_virid
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 12
+#SBATCH --mem=200G
+#SBATCH -o ${log}/%x.%j.out
+#SBATCH -e ${log}/%x.%j.err
+
+database="viridiplantae_odb12"
+date
+echo "[M]: Host Name: `hostname`"
+echo "[M]: Beginning BUSCO analysis of ${baseprim} against database ${database}"
+#Module files
+source /home/FCAM/msmith/busco/.venv/bin/activate
+module load blast/2.7.1 augustus/3.6.0 hmmer/3.3.2 R/4.2.2 java/17.0.2 bbmap/39.08 prodigal/2.6.3
+export AUGUTUS_CONFIG_PATH="/core/projects/EBP/smith/busco/config"
+threads="$(getconf _NPROCESSORS_ONLN)"
+export PATH="/home/FCAM/msmith/R/x86_64-pc-linux-gnu-library/4.2:$PATH"
+export PATH="/core/projects/EBP/smith/bin/miniprot:$PATH"
+outbusco=${outdir}/busco
+
+busco --offline -c ${threads} -i ${prim} -m "genome" -f -l ${database} -o "prim_mancur_${database}" --out_path ${outbusco}
+if [[ $? -eq 0 ]] ; then
+echo "[M]: Done."
+exit 0
+else
+echo "[E]: BUSCO run failed. Exit code $?"
+fi
+HEADER`
+
+##Embryophyta----
+jid_busco_emb=`sbatch <<- HEADER | egrep -o -e "\b[0-9]+$"
+#!/bin/bash
+#SBATCH -J busco_emb
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 12
+#SBATCH --mem=200G
+#SBATCH -o ${log}/%x.%j.out
+#SBATCH -e ${log}/%x.%j.err
+
+database="embryophyta_odb12"
+date
+echo "[M]: Host Name: `hostname`"
+echo "[M]: Beginning BUSCO analysis of ${baseprim} against database ${database}"
+#Module files
+source /home/FCAM/msmith/busco/.venv/bin/activate
+module load blast/2.7.1 augustus/3.6.0 hmmer/3.3.2 R/4.2.2 java/17.0.2 bbmap/39.08 prodigal/2.6.3
+export AUGUTUS_CONFIG_PATH="/core/projects/EBP/smith/busco/config"
+threads="$(getconf _NPROCESSORS_ONLN)"
+export PATH="/home/FCAM/msmith/R/x86_64-pc-linux-gnu-library/4.2:$PATH"
+export PATH="/core/projects/EBP/smith/bin/miniprot:$PATH"
+outbusco=${outdir}/busco
+
+busco --offline -c ${threads} -i ${prim} -m "genome" -f -l ${database} -o "prim_mancur_${database}" --out_path ${outbusco}
+if [[ $? -eq 0 ]] ; then
+echo "[M]: Done."
+exit 0
+else
+echo "[E]: BUSCO run failed. Exit code $?"
+fi
+HEADER`
+
+
+#KMERS----
+jid_kmers=`sbatch <<- HEADER | egrep -o -e "\b[0-9]+$"
+#!/bin/bash
+#SBATCH -J kmers
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 10
+#SBATCH --mem=100G
+#SBATCH -o ${log}/%x.%j.out
+#SBATCH -e ${log}/%x.%j.err
+
+date
+echo "[M]: Host Name: `hostname`"
+#Module files
+module load R/4.2.2 meryl/1.4.1 merqury/1.3
+export PATH="/home/FCAM/msmith/R/x86_64-pc-linux-gnu-library/4.2:$PATH"
+export PATH="/core/projects/EBP/smith/bin/genomescope2.0:$PATH"
+##sub_merqury="/home/FCAM/smith/_submit_merqury.sh"
+outmerq=${outdir}/merqury
+if [[ ! -d ${outmerq} ]] ; then
+  mkdir ${outmerq}
+fi
+outfix=${outmerq}/prim_mancur_kmers
+
+sub_merqury="/home/FCAM/smith/_submit_merqury.sh"
+${sub_merqury} "${outfix}.meryl" ${prim} ${alt} ${output}
+if [[ $? -eq 0 ]] ; then
+echo "[M]: Done."
+exit 0
+else
+echo "[E]: Merqury run failed. Exit code $?"
+fi
+
+HEADER`
+
+echo "[M]: All jobs submitted."
+exit
