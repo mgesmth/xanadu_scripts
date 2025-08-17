d8e66a947f8d0c91b35ad3059448e7d7dd0c1af9
Meg Smith <meg8130@student.ubc.ca>
2025-08-17 10:49:29 -0700

    Update categorize_svs.py

--- categorize_svs.py
+++ categorize_svs.py
@@ -109,67 +109,65 @@ with open("sv_allele_summary.tsv") as f, open("tmpnon_inverted_equal_lengths.tsv
 			coast_allele=int(columns[4])
 			coast_len=int(columns[7])
 			inversion = bool(int(columns[8]))len_string=str(prim_len) + ":" + str(alt_len) + ":" + str(coast_len)
-
-            #There's definitely a more efficient way to do this, but here we are
-
-            #pseudo-genotype for convenience
-            if alt_allele == 0 and coast_allele == 1:
-                genotype="0:0:1"
-            elif alt_allele == 1 and coast_allele == 0:
-                genotype="0:1:0"
-            elif alt_allele == 1 and coast_allele == 1:
-                genotype="0:1:1"
-            else:
-                genotype="0:1:2"
-
-                #Categorize variants based on variant genotype
-            if genotype == "0:0:1":
-                if inversion is False:
-                    if not handle_twollele_indel(prim_len, coast_len, line, ef):
-			continue
-                elif inversion is True:
-                    handle_twoallele_inversion(prim_len, coast_len)
-                else:
-                    raise Exception("[E]: Inversion Boolean not recognized.")
-            elif genotype == "0:1:0":
-                if inversion is False:
-                    if not handle_twollele_indel(prim_len, alt_len, line, ef):
-			continue
-                elif inversion is True:
-                    handle_twoallele_inversion(prim_len, alt_len)
-                else:
-                    raise Exception("[E]: Inversion Boolean not recognized.")
-            elif genotype == "0:1:1":
-                if inversion is False:
-                    if not handle_twollele_indel(prim_len, alt_len, line, ef):
-			continue
-                elif inversion is True:
-                    handle_twoallele_inversion(prim_len, alt_len)
-                else:
-                    raise Exception("[E]: Inversion Boolean not recognized.")
-            elif genotype == "0:1:2":
-                #Implicit in there being three alleles is that there is some indel activity
-                longest_allele=max(prim_len, alt_len, coast_len)
-                shortest_allele=min(prim_len, alt_len, coast_len)
-                if inversion is False:
-                    if longest_allele == prim_len:
-                        first_category="DEL"
-                        second_category="DEL"
-                    elif shortest_allele == prim_len:
-                        first_category="INS"
-                        second_category="INS"
-                    else: #else prim is the mid-length allele
-                        first_category="INS"
-                        second_category="DEL"
-                elif inversion is True:
-                    first_category="INV"
-                    if longest_allele == prim_len:
-                        second_category="DEL"
-                    elif shortest_allele == prim_len:
-                        second_category="INS"
-                    else: #else prim is the mid-length allele
-                        second_category="INDEL"
-            else:
-                raise Exception("[E]: Genotype not recognized.")
-            newline=(columns[0],columns[1],columns[2],first_category,second_category,genotype,len_string)
-            fw.write('\t'.join(map(str, newline)) + '\n')
+			#There's definitely a more efficient way to do this, but here we are - pseudo-genotype for convenience
+			if alt_allele == 0 and coast_allele == 1:
+				genotype="0:0:1"
+			elif alt_allele == 1 and coast_allele == 0:
+				genotype="0:1:0"
+			elif alt_allele == 1 and coast_allele == 1:
+				genotype="0:1:1"
+			else:
+				genotype="0:1:2"
+				
+			#Categorize variants based on variant genotype
+			if genotype == "0:0:1":
+				if inversion is False:
+					if not handle_twollele_indel(prim_len, coast_len, line, ef):
+						continue
+				elif inversion is True:
+					handle_twoallele_inversion(prim_len, coast_len)
+				else:
+					raise Exception("[E]: Inversion Boolean not recognized.")
+			elif genotype == "0:1:0":
+				if inversion is False:
+					if not handle_twollele_indel(prim_len, alt_len, line, ef):
+						continue
+				elif inversion is True:
+					handle_twoallele_inversion(prim_len, alt_len)
+				else:
+					raise Exception("[E]: Inversion Boolean not recognized.")
+			elif genotype == "0:1:1":
+				if inversion is False:
+					if not handle_twollele_indel(prim_len, alt_len, line, ef):
+						continue
+				elif inversion is True:
+					handle_twoallele_inversion(prim_len, alt_len)
+				else:
+					raise Exception("[E]: Inversion Boolean not recognized.")
+			elif genotype == "0:1:2":
+				#Implicit in there being three alleles is that there is some indel activity
+				longest_allele=max(prim_len, alt_len, coast_len)
+				shortest_allele=min(prim_len, alt_len, coast_len)
+				if inversion is False:
+					if longest_allele == prim_len:
+						first_category="DEL"
+						second_category="DEL"
+					elif shortest_allele == prim_len:
+						first_category="INS"
+						second_category="INS"
+					else: #else prim is the mid-length allele
+						first_category="INS"
+						second_category="DEL"
+				elif inversion is True:
+					first_category="INV"
+					if longest_allele == prim_len:
+						second_category="DEL"
+					elif shortest_allele == prim_len:
+						second_category="INS"
+					else: #else prim is the mid-length allele
+						second_category="INDEL"
+				else:
+					raise Exception("[E]: Genotype not recognized.")
+			
+			newline=(columns[0],columns[1],columns[2],first_category,second_category,genotype,len_string)
+			fw.write('\t'.join(map(str, newline)) + '\n')
