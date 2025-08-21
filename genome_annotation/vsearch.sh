3680e9b84e2b6299f0c56b3f1c7402ae1b32aeb7
Meg Smith <meg8130@student.ubc.ca>
2025-08-21 09:54:14 -0700

    Create vsearch.sh

---
+++ genome_annotation/vsearch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J vsearch
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 12
+#SBATCH --mem=48G
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=meg8130@student.ubc.ca
+#SBATCH -o %x.%j.out
+#SBATCH -e %x.%j.err
+
+set -e
+date
+echo "[M]: Host Name: `hostname`"
+dir=/home/FCAM/msmith/transcriptome/01_transcriptome_alignment
+cat_fa=${dir}/cat_transcripts.fasta
+centroids_out=${dir}/centroids_clustered.fasta
+uc_out=${dir}/uc_clustered.uc
+
+#Cluster search with VSearch
+module load vsearch/2.22.1
+vsearch --threads 12 --cluster_fast "$cat_fa" --centroids "$centroids_out" --uc "$uc_out"
