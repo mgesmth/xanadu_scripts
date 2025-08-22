944e141799a3438f1bfac6803acd41554dae6b43
Meg Smith <meg8130@student.ubc.ca>
2025-08-22 12:51:43 -0400

    commit

--- genome_annotation/vsearch.sh
+++ genome_annotation/vsearch.sh
@@ -14,9 +14,10 @@ date
 echo "[M]: Host Name: `hostname`"
 dir=/home/FCAM/msmith/transcriptome/01_transcriptome_alignment
 cat_fa=${dir}/cat_transcripts.fasta
-centroids_out=${dir}/centroids_clustered.fasta
-uc_out=${dir}/uc_clustered.uc
+centroids_out=${dir}/centroids_clustered_2.4.3.fasta
+uc_out=${dir}/uc_clustered_2.4.3.uc
 
 #Cluster search with VSearch
-module load vsearch/2.22.1
+#module load vsearch/2.22.1
+module load vsearch/2.4.3
 vsearch --threads 12 --cluster_fast "$cat_fa" --centroids "$centroids_out" --uc "$uc_out" --id 0.95
