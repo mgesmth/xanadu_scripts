98eebf6da41558741a317bc40947721340c6895a
Meg Smith <meg8130@student.ubc.ca>
2025-08-31 17:18:52 -0700

    Break up quality stats script

---
+++ post_asm_analysis/kmers.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH -J kmers
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 4
+#SBATCH --mem=10G
+#SBATCH -o ${log}/%x.%j.out
+#SBATCH -e ${log}/%x.%j.err
+
+set -e
+date
+echo "[M]: Host Name: `hostname`"
+
+#variables
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+outdir=${core}/manual_curation_files
+prim=${outdir}/interior_primary_final_mancur.fa
+baseprim=$(basename ${prim})
+alt=${core}/CBP_assemblyfiles/interior_alternate_final.fa
+
+export PATH="${home}/scripts/post_asm_analysis:$PATH"
+log=${core}/manual_curation_files/log
+
+#Module files
+module load R/4.2.2 meryl/1.4.1 merqury/1.3
+export PATH="/home/FCAM/msmith/R/x86_64-pc-linux-gnu-library/4.2:$PATH"
+export PATH="/core/projects/EBP/smith/bin/genomescope2.0:$PATH"
+outmerq=${outdir}/merqury
+outfix=${outmerq}/prim_mancur_kmers
+sub_merqury=${outdir}/merqury/_submit_merqury.sh
+
+${sub_merqury} "${outfix}.meryl" ${prim} ${alt} ${output}
+if [[ $? -eq 0 ]] ; then
+echo "[M]: Done."
+exit 0
+else
+echo "[E]: Merqury run failed. Exit code $?"
+fi
---
+++ post_asm_analysis/quast.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH -J quast
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 12
+#SBATCH --mem=150G
+#SBATCH -o ${log}/%x.%j.out
+#SBATCH -e ${log}/%x.%j.err
+
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+outdir=${core}/manual_curation_files
+prim=${outdir}/interior_primary_final_mancur.fa
+baseprim=$(basename ${prim})
+alt=${core}/CBP_assemblyfiles/interior_alternate_final.fa
+export PATH="${home}/scripts/post_asm_analysis:$PATH"
+log=${core}/manual_curation_files/log
+
+date
+echo "[M]: Beginning QUAST analysis of ${baseprim}"
+module load quast/5.2.0
+quast=/isg/shared/apps/quast/5.2.0/quast.py
+threads="$(getconf _NPROCESSORS_ONLN)"
+outquast=${outdir}/quast
+
+python3 $quast -t ${threads} --split-scaffolds --large -o ${outquast} ${prim}
+if [[ $? -eq 0 ]] ; then
+echo "[M]: Done."
+exit 0
+else
+echo "[E]: QUAST run failed. Exit code $?"
+fi
