aae87d4e58eb26d59df5ce3b9e11afe2bac5aab0
Meg Smith <meg8130@student.ubc.ca>
2025-09-04 12:26:43 -0700

    Create pangenome_graph_generation.sh

---
+++ post_asm_analysis/pangenome_graph_generation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+date
+set -e
+echo "[M]: Host Name: `hostname`"
+echo "[M]: Beginning minigraph graph generation"
+
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+prim=${scratch}/interior_primary_final_mancur_bigscaffoldsplit.fa
+alt=${scratch}/interior_alternate_1Mb.fa
+coast=${scratch}/coastal_1Mb.fa
+prx="final_finalpangenome"
+outdir=${core}/manual_curation_files/minigraph
+threads="$(getconf _NPROCESSORS_ONLN)"
+prx="final_finalpangenome"
+outdir=${core}/manual_curation_files/minigraph
+
+#executables
+module load zlib/1.2.11
+export PATH="${core}/bin/minigraph-0.21:$PATH"
+export PATH="${core}/bin/gfatools:$PATH"
+
+#pangenome graph:
+minigraph -cxggs -t 24 ${prim} ${alt} ${coast} > "${outdir}/${prx}.gfa"
+#call bubbles (SVs)
+gfatools bubble "${outdir}/${prx}.gfa" > "${outdir}/${prx}_unfiltered.bed"
+#get stats on the pangenome graph (number of nodes/edges, etc.)
+gfatools stat "${outdir}/${prx}.gfa" > "${outdir}/${prx}.stat"
+
+if [[ $? -eq 0 ]] ; then
+echo "[M]: Done."
+exit 0
+else
+echo "[E]: Graph generation failed. Exit code $?"
+fi
