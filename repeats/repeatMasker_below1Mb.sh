0d322a573bcb6bfac45c124a9edb3a528873a9af
Meg Smith <meg8130@student.ubc.ca>
2025-09-07 13:44:41 -0700

    Create repeatMasker_below1Mb.sh

---
+++ repeats/repeatMasker_below1Mb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -J repeatMasker
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 3
+#SBATCH --mem=8G
+#SBATCH --array=[0-1600]
+#SBATCH -o %x.%a.%A.out
+#SBATCH -e %x.%a.%A.err
+
+echo "[M]: Host Name: `hostname`"
+
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+repdir=${home}/repeats_mancur/above_1Mb
+db=${home}/repeats/primary_db
+tetools=${core}/bin/dfam-tetools-latest.sif
+
+cd ${repdir}
+
+files=($(cat below_files.txt))
+file=${files[$SLURM_ARRAY_TASK_ID]}
+
+singularity exec $tetools \
+RepeatMasker -frag 60000000 -pa 6 -gff -q -dir ${repdir} -lib "${db}/primary-families.fa" "${file}"
