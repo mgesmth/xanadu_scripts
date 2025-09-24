d19391b1f5dd60861a02cdd7592cea0acfc87fb9
Meg Smith <meg8130@student.ubc.ca>
2025-09-24 16:53:28 -0700

    Create fastqc_sedata.sh

---
+++ genome_annotation/fastqc_sedata.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -J fastqc
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 24
+#SBATCH --mem=150G
+#SBATCH -o %x.%j.out
+#SBATCH -e %x.%j.err
+
+set -e
+date
+echo "[M]: Host Name: `hostname`"
+
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+se_dir=${core}/se_reads
+fastq_dir=${se_dir}/fastqc
+
+module load fastqc/0.12.1
+module load MultiQC/1.29
+
+cd $fastq_dir
+
+files=$(ls *.fastq.gz)
+fastqc -o ./ -t 24 ${files}
+multiqc .
