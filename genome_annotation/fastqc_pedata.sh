a61dcd72719a263223d16ca8013414fac262e311
Meg Smith <meg8130@student.ubc.ca>
2025-09-25 09:55:07 -0700

    Update fastqc_pedata.sh

--- genome_annotation/fastqc_pedata.sh
+++ genome_annotation/fastqc_pedata.sh
@@ -2,6 +2,7 @@
 #SBATCH -J fastqc
 #SBATCH -p general
 #SBATCH -q general
+#SBATCH -d afterok:792458
 #SBATCH -c 24
 #SBATCH --mem=150G
 #SBATCH -o %x.%j.out
@@ -14,7 +15,7 @@ echo "[M]: Host Name: `hostname`"
 home=/home/FCAM/msmith
 core=/core/projects/EBP/smith
 scratch=/scratch/msmith
-pe_dir=${core}/genome_annotation_shortread_data/pe_dir
+pe_dir=${core}/genome_annotation_shortread_data/pe_reads
 fastq_dir=${pe_dir}/fastqc
 
 module load fastqc/0.12.1
