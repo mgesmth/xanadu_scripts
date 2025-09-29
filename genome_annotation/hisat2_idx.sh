df0300da16177fbb5d726d893f4771fe26201421
Meg Smith <meg8130@student.ubc.ca>
2025-09-29 15:26:09 -0700

    Update hisat2_idx.sh

--- genome_annotation/hisat2_idx.sh
+++ genome_annotation/hisat2_idx.sh
@@ -16,10 +16,10 @@ core=/core/projects/EBP/smith
 scratch=/scratch/msmith
 splitdir=${core}/manual_curation_files/mancur_masked_split
 hisatdir=${home}/transcriptome/02_braker_annotation/hisat2/masked_idx
-scaffolds=$(cat ${splitdir}/names_rightorder_500kb.csv)
-#names_rightorder_500kb is a comma-separated list of files containing scaffolds larger than 500kb, 1 to 251, which were split using seqkit split
+scaffolds=$(cat ${splitdir}/new_names_rightorder_500kb.csv)
+#names_rightorder_500kb is a comma-separated list of files (with abs. path) containing scaffolds larger than 500kb, 1 to 251, which were split using seqkit split
 
-module load hisat2/2.2.1 
+module load hisat2/2.2.1
 
 cd ${hisatdir}
 hisat2-build -p 12 -f ${scaffolds} "interior_primary_mancur_masked"
