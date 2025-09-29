fbbc10225c2575ec7006d7dc820221036e885193
Meg Smith <meg8130@student.ubc.ca>
2025-09-29 15:19:05 -0700

    Create hisat2_idx.sh

---
+++ genome_annotation/hisat2_idx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J hisat2_idx
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 12
+#SBATCH --mem=64G
+#SBATCH -o %x.%j.out
+#SBATCH -e %x.%j.err
+
+set -e
+date
+echo "[M]: Host Name: `hostname`"
+
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+splitdir=${core}/manual_curation_files/mancur_masked_split
+hisatdir=${home}/transcriptome/02_braker_annotation/hisat2/masked_idx
+scaffolds=$(cat ${splitdir}/names_rightorder_500kb.csv)
+
+cd ${hisatdir}
+hisat2-build -p 12 -f ${scaffolds} "interior_primary_mancur_masked"
