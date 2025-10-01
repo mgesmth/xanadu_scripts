86ac264be04842f9ab24e6f1b5f35688c01dbe6b
Meg Smith <meg8130@student.ubc.ca>
2025-10-01 16:54:57 -0700

    Update hisat2_aln.sh

--- genome_annotation/hisat2_aln.sh
+++ genome_annotation/hisat2_aln.sh
@@ -28,16 +28,16 @@ module load samtools/1.19
 ls ${petrimdir}/*_trim_R1_paired.fastq.gz > pe_trim.txt
 
 for R1 in $(cat pe_trim.txt) ; do
-  R2=$(echo "$R1" | sed 's/R1/R2/g')
-  base=${R1/_R1.fastq.gz/}
+  R2=$(echo "$R1" | sed 's/_R1/_R2/g')
+  base=$(basename ${R1/_R1.fastq.gz/})
   hisat2 -p 18 --max-intronlen 2000000 -x ${idx} -1 ${R1} -2 ${R2} | \
-  samtools sort -m 2G -@ 18 -O bam > ${outdir}/${base}_paired.bam
+  samtools sort -m 2G -@ 18 -O bam -o ${outdir}/${base}_paired.bam
 done && rm pe_trim.txt
 
 ls ${setrimdir}/*_trim.fastq.gz > se_trim.txt
 
 for file in $(cat se_trim.txt) ; do
-  base=${R1/.fastq.gz/}
+  base=$(basename ${file/.fastq.gz/})
   hisat2 -p 18 --max-intronlen 2000000 -x ${idx} -U ${file} | \
   samtools sort -m 2G -@ 18 -O bam > ${outdir}/${base}.bam
 done && rm se_trim.txt
