43a0501757f15e7f55dd266e0ed1b8bfde3b9a8b
Meg Smith <meg8130@student.ubc.ca>
2025-10-01 15:58:43 -0700

    Create hisat2_aln.sh

---
+++ genome_annotation/hisat2_aln.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH -J hisat2
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 36
+#SBATCH --mem=300G
+#SBATCH -o %x.%j.out
+#SBATCH -e %x.%j.err
+
+set -e
+date
+echo "[M]: Host Name: `hostname`"
+
+home=/home/FCAM/msmith
+scratch=/scratch/msmith
+core=/core/projects/EBP/smith
+idx=${home}/transcriptome/02_braker_annotation/hisat2/masked_idx/interior_primary_mancur_masked
+petrimdir=${core}/genome_annotation_shortread_data/pe_reads_trim
+setrimdir=${core}/genome_annotation_shortread_data/se_reads_trim
+outdir=${core}/genome_annotation_shortread_data/alignments
+if [[ ! -d $outdir ]] ; then
+  mkdir $outdir
+fi
+
+module load hisat2/2.2.1
+module load samtools/1.19
+
+ls ${petrimdir}/*_trim_R1_paired.fastq.gz > pe_trim.txt
+
+for R1 in $(cat pe_trim.txt) ; do
+  R2=$(echo "$R1" | sed 's/R1/R2/g')
+  base=${R1/_R1.fastq.gz/}
+  hisat2 -p 18 --max-intronlen 2000000 -x ${idx} -1 ${R1} -2 ${R2} | \
+  samtools sort -m 2G -@ 18 -O bam > ${outdir}/${base}_paired.bam
+done && rm pe_trim.txt
+
+ls ${setrimdir}/*_trim.fastq.gz > se_trim.txt
+
+for file in $(cat se_trim.txt) ; do
+  base=${R1/.fastq.gz/}
+  hisat2 -p 18 --max-intronlen 2000000 -x ${idx} -U ${file} | \
+  samtools sort -m 2G -@ 18 -O bam > ${outdir}/${base}.bam
+done && rm se_trim.txt
