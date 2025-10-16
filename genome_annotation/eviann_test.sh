9c15537ccf3b8a4a82e01ca6360736328df0be3c
Meg Smith <meg8130@student.ubc.ca>
2025-10-16 13:54:17 -0700

    Update eviann_test.sh

--- genome_annotation/eviann_test.sh
+++ genome_annotation/eviann_test.sh
@@ -14,11 +14,14 @@ echo "[M]: Host Name: `hostname`"
 home=/home/FCAM/msmith
 core=/core/projects/EBP/msmith
 genome=${core}/manual_curation_files/interior_primary_mancur_masked_500kb.fa
-transcript_file=${core}/genome_annotation_isoseq_data/test/test_eviann_evidence.txt
-protein_db=${core}/genome_annotation_isoseq_data/test/conifer_proteins_test.faa
+workdir=${core}/genome_annotation_isoseq_data/test
+transcript_file=${workdir}/test_eviann_evidence.txt
+protein_db=${workdir}/conifer_proteins_test.faa
+
+cd $workdir
 
 export PATH="${core}/bin/EviAnn-2.0.4/bin:$PATH"
 module load minimap2/2.28 hisat2/2.2.1 samtools/1.19
 #all other dependencies are within the Eviann package itself
 
-eviann.sh -t 10 -g ${genome} -r ${transcript_file} -p ${protein_db}
+eviann.sh -t 10 -g ${genome} -r ${transcript_file} -p ${protein_db} -m 100000
