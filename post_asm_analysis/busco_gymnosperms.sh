532df5b2b92781d30c06f2f9a3fdc457113121da
Meg Smith <meg8130@student.ubc.ca>
2025-10-23 14:41:49 -0700

    commit

---
+++ post_asm_analysis/busco_gymnosperms.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH -J busco
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 12
+#SBATCH --mem=200G
+#SBATCH -o /core/projects/EBP/smith/manual_curation_files/log/%x.%j.out
+#SBATCH -e /core/projects/EBP/smith/manual_curation_files/log/%x.%j.err
+
+set -e
+
+echo "[M]: Host Name: `hostname`"
+
+#variables
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+outdir=${core}/manual_curation_files
+asm=$1
+baseasm=$(basename ${asm})
+
+export PATH="${home}/scripts/post_asm_analysis:$PATH"
+log=${core}/manual_curation_files/log
+
+database=${home}/busco/busco_downloads/gymnosperm_odb10
+date
+echo "[M]: Beginning BUSCO analysis of ${baseasm} against database ${database}"
+#Module files
+source /home/FCAM/msmith/busco/.venv/bin/activate
+module load blast/2.7.1 augustus/3.6.0 hmmer/3.3.2 R/4.2.2 java/17.0.2 bbmap/39.08 prodigal/2.6.3
+export AUGUTUS_CONFIG_PATH="/core/projects/EBP/smith/busco/config"
+threads="$(getconf _NPROCESSORS_ONLN)"
+export PATH="/home/FCAM/msmith/R/x86_64-pc-linux-gnu-library/4.2:$PATH"
+export PATH="/core/projects/EBP/smith/bin/miniprot:$PATH"
+outbusco=${outdir}/busco
+
+busco -c ${threads} -i ${asm} -m "genome" -f -l ${database} -o "asm_${database}" --out_path ${outbusco}
+if [[ $? -eq 0 ]] ; then
+echo "[M]: Done."
+exit 0
+else
+echo "[E]: BUSCO run failed. Exit code $?"
+fi
