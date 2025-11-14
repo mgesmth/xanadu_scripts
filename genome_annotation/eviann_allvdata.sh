085ebe9fd76fc33392e85b655bfe412394569c08
Meg Smith <meg8130@student.ubc.ca>
2025-11-13 21:21:27 -0800

    Update eviann_allvdata.sh

--- genome_annotation/eviann_allvdata.sh
+++ genome_annotation/eviann_allvdata.sh
@@ -3,7 +3,7 @@
 #SBATCH -p general
 #SBATCH -q general
 #SBATCH -c 36
-#SBATCH --mem=500G
+#SBATCH --mem=200G
 #SBATCH -o %x.%j.out
 #SBATCH -e %x.%j.err
 
@@ -13,18 +13,23 @@ echo "[M]: Host Name: `hostname`"
 
 home=/home/FCAM/msmith
 core=/core/projects/EBP/smith
-genome=${core}/manual_curation_files/interior_primary_mancur_masked_500kb.fa
-transcript_file=${home}/transcriptome/03_eviann_annotation/evidence_allintsamp.txt
+int_genome=${core}/manual_curation_files/interior_primary_mancur_masked_500kb.fa
+coa_genome=${core}/coastal/coastal_masked_500kb.fa
+transcript_file=${home}/transcriptome/03_eviann_annotation/evidence_allv.txt
 protein_db=${home}/transcriptome/02_braker_annotation/conifer_geneSet_protein_v2_150.faa
-workdir=${core}/eviann_allintsamp_new
-if [[ ! -d ${workdir} ]] ; then
-  mkdir ${workdir}
-fi
-
-cd ${workdir}
+int_workdir=${core}/eviann/eviann_int_allvdata
+coa_workdir=${core}/eviann/eviann_coa_allvdata
 
 export PATH="${core}/bin/EviAnn-2.0.4/bin:$PATH"
 module load minimap2/2.28 hisat2/2.2.1 samtools/1.19
 #all other dependencies are within the Eviann package itself
 
-eviann.sh -t 36 -g ${genome} -r ${transcript_file} -p ${protein_db} -m 100000
+cd ${int_workdir}
+
+#interior
+eviann.sh -t 36 -g ${int_genome} -r ${transcript_file} -p ${protein_db} -m 100000
+
+cd ${coa_workdir}
+
+#coastal 
+eviann.sh -t 36 -g ${coa_genome} -r ${transcript_file} -p ${protein_db} -m 100000
