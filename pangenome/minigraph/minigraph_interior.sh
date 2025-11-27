e2eb00da208d305ceb4021e4c4af03bdb2467f2d
Meg Smith <meg8130@student.ubc.ca>
2025-11-27 13:32:45 -0800

    Update minigraph_interior.sh

--- pangenome/minigraph/minigraph_interior.sh
+++ pangenome/minigraph/minigraph_interior.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=minigraph
 #SBATCH --partition=himem2
 #SBATCH --qos=himem
-#SBATCH --cpus-per-task=24
-#SBATCH --mem=650G
+#SBATCH --cpus-per-task=36
+#SBATCH --mem=1250G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=meg8130@student.ubc.ca
 #SBATCH -o minigraph_interior.%j.out
@@ -13,31 +13,56 @@ echo `hostname`
 
 home=/home/FCAM/msmith
 core=/core/projects/EBP/smith
-prim=/scratch/msmith/interior_primary_first15.fa
+prim=${core}/manual_curation_files/interior_primary_final_mancur_1Mb.fa
 alt=${core}/CBP_assemblyfiles/interior_alternate_1Mb.fa
-out=${home}/minigraph_out/interior_primalt_newopts
+outdir=${home}/minigraph_justint/interior_pangenome
+outfix="interior_pangenome"
+out=${home}/minigraph_justint/${outfix}
 
-#test - succeeded
-#test=${core}/bin/minigraph/test
-#test_gfa=${test}/MT.gfa
-#human=${test}/MT-human.fa
-#chimp=${test}/MT-chimp.fa
-#orang=${test}/MT-orangA.fa
-#test_out=${test}/test
+cd $outdir
 
 #EXEC
 export PATH="${core}/bin/zlib-1.3.1:$PATH"
-export PATH="${core}/bin/minigraph:$PATH"
+module load java/17.0.2
+module load bedtools/2.29.0
+export PATH="${core}/bin/minigraph-0.21:$PATH"
+export PATH="${core}/bin/minigraph-0.21/mg-cookbook-v1_x64-linux:$PATH"
 export PATH="${core}/bin/gfatools:$PATH"
+k8_dir=${core}/bin/minigraph-0.21/mg-cookbook-v1_x64-linux
+misc_dir=${core}/bin/minigraph-0.21/misc
 
-#test real variables - succeeded
-#ls ${prim}
-#ls ${alt}
-#echo ${out}
+minigraph -cxggs -t 36 ${prim} ${alt} > "${out}.gfa"
+gfatools bubble "${out}.gfa" > "${out}_unfiltered.bed"
+gfatools stat "${out}.gfa" > "${out}.stat"
+minigraph -cxasm --call -t 36 "${out}.gfa" ${prim} > "${out}_primcall.bed"
+minigraph -cxasm --call -t 36 "${out}.gfa" ${alt} > "${out}_altcall.bed"
 
-#test command
-#minigraph -cxggs -l 10k ${test_gfa} ${human} ${chimp} ${orang} > "${test_out}.gfa"
-#gfatools bubble "${test_out}.gfa" > "${test_out}.bed"
+primcall_prefix=$(basename "${out}_primcall.bed" | sed 's/.bed//')
+altcall_prefix=$(basename "${out}_altcall.bed" | sed 's/.bed//')
+mkdir minigraph_tmp
+cd minigraph_tmp
+cp "${out}_primcall.bed" .
+cp "${out}_altcall.bed" .
 
-minigraph -cxggs -t 24 -k21 --min-cov-mapq 20 -q 20 --gg-match-pen 10 ${prim} ${alt} > "${out}.gfa"
-gfatools bubble "${out}.gfa" > "${out}.bed"
+if [[ -f "${out}_primcall.bed" && -f "${out}_altcall.bed" ]] ; then
+  echo -e "${outfix}_primcall.bed\n${outfix}_altcall.bed" > samples.txt
+  bed_rightorder=$(echo "${primcall_prefix}.bed ${altcall_prefix}.bed")
+  paste ${bed_rightorder} | ${k8_dir}/k8 ${misc_dir}/mgutils.js merge -s samples.txt - | gzip -c > "${outfix}.sv.bed.gz"
+  ${k8_dir}/k8 ${misc_dir}/mgutils-es6.js merge2vcf -a1 -r0 "${outfix}.sv.bed.gz" > "${out}_unfiltered.sv.vcf"
+
+  #filter vcf
+  unfvcf="${outdir}/${prx}_unfiltered.sv.vcf"
+  awk '/^#/ {print} !/^#/ && $10 != "." && $11 != "." && $12 != "." {print}' ${unfvcf} | \
+  awk '/^#/ {print} !/^#/ && $11 ~ /1:1/ || $12 ~ /1:1/ {print}' > "${out}_filtered1.sv.vcf"
+  cd ..
+  rm -r minigraph_tmp
+
+  #filter other files
+  awk 'BEGIN { OFS="\t" } /^#/ {next} !/^#/ {
+    split($8,m,";")
+    split(m[1],n,"=")
+    print $1,$2,n[2] }' "${out}_filtered1.sv.vcf" > filtered_coordinates.bed
+
+  bedtools intersect -F 1 -wa -a "${out}_primcall.bed" -b filtered_coordinates.bed  > "${out}_primcall_filtered.bed" && \
+  bedtools intersect -F 1 -wa -a "${out}_altcall.bed" -b filtered_coordinates.bed > "${out}_altcall_filtered.bed" && \
+  bedtools intersect -F 1 -wa -a "${out}_unfiltered.bed" -b filtered_coordinates.bed > "${out}_filtered.bed"
