b642410b2c8676a355e2a0bc995a2e818e5a6774
Meg Smith <meg8130@student.ubc.ca>
2025-11-28 16:03:57 -0800

    Update align_split_hic.sh

--- manual_curation/manual_curation_round2/align_split_hic.sh
+++ manual_curation/manual_curation_round2/align_split_hic.sh
@@ -11,8 +11,7 @@
 
 set -e
 
-date
-echo "[M]: Host Name: `hostname`"
+echo "`date`[M]: Host Name: `hostname`"
 module load bwa/0.7.17
 module load samtools/1.19
 
@@ -21,21 +20,8 @@ scratch=/scratch/msmith
 core=/core/projects/EBP/smith
 fq_dir=${scratch}/hic_split
 bam_dir=${scratch}/hic_bams
-#if [[ ! -d "$bam_dir" ]] ; then
-#  mkdir "$bam_dir"
-#fi
 ref=${core}/CBP_assemblyfiles/interior_primary_final.fa
 ref_name=$(basename ${ref})
-#if [ ! -f "${assembly}.bwt" ]; then
-#  echo "[M]: BWA index not found. Indexing..."
-#  bwa index ${ref}
-#  if [[ $? -ne 0 ]] ; then
-#    echo "[E]: Indexing failed. Exit code $?."
-#    exit 1
-#  else
-#    echo "[M]: Indexing complete."
-#  fi
-#fi
 
 cd ${fq_dir}
 fqs=($(cat fastqs.txt))
@@ -49,25 +35,12 @@ libraryName="HiC_library"
 
 rg="@RG\\tID:${name}\\tSM:${sampleName}\\tPL:LS454\\tLB:${libraryName}"
 
-echo "[M]: Welcome to task ${SLURM_ARRAY_TASK_ID}."
-echo "[M]: We are aligning ${r1} and ${r2} to ${ref_name}."
+echo -e "`date`[M]: Welcome to task ${SLURM_ARRAY_TASK_ID}."
+echo -e "`date`[M]: We are aligning ${r1} and ${r2} to ${ref_name}.\n"
 
 bwa mem -SP5M -t 4 -R "$rg" "$ref" "$r1" "$r2" | \
 samtools sort -n -@ 4 -m 2500M -O "bam" -o "${bam_dir}/${out}"
 
-if [[ $? -eq 0 ]] ; then
-  date
-  echo "[M]: Alignment complete. Removing fastqs for disk..."
-#  rm "$r1" "$r2"
-  if [[ $? -eq 0 ]] ; then
-    echo "[M]: All cleaned up. Bye."
-    exit 0
-  else
-    echo "[E]: Failed to remove fastqs."
-    exit 1
-  fi
-else
-  echo "[E]: Alignment failed. Exit code $?"
-  date
-  exit 1
-fi
+echo -e "\n`date`:[M]: Alignment complete. Removing fastqs for disk..."
+rm "$r1" "$r2"
+echo -e "\n`date`:[M]: Done."
