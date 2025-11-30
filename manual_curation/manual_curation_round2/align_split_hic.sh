116761482dd85454075da0713015dff6b0840a5b
Meg Smith <meg8130@student.ubc.ca>
2025-11-30 11:51:44 -0800

    Update align_split_hic.sh

--- manual_curation/manual_curation_round2/align_split_hic.sh
+++ manual_curation/manual_curation_round2/align_split_hic.sh
@@ -5,7 +5,7 @@
 #SBATCH -n 1
 #SBATCH -c 10
 #SBATCH --mem=40G
-#SBATCH --array=[0-298]%20
+#SBATCH --array=[0-298]
 #SBATCH -o %x.%A.%a.out
 #SBATCH -e %x.%A.%a.err
 
@@ -43,7 +43,7 @@ echo -e "`date`[M]: Welcome to task ${SLURM_ARRAY_TASK_ID}."
 echo -e "`date`[M]: We are aligning ${r1} and ${r2} to ${ref_name}.\n"
 
 bwa mem -SP5M -t 8 -R "$rg" "$ref" "$r1" "$r2" | samtools view -b -o "${bam_dir}/${name//.gz/}.unsorted.bam" -
-samtools sort -n -@ 8 -m 2500M -O "bam" -o "${bam_dir}/${out}" "${bam_dir}/${name//.gz/}.unsorted.bam" && rm "${bam_dir}/${name//.gz/}.unsorted.bam"
+samtools sort -n -@ 8 -m 2000M -O "bam" -o "${bam_dir}/${out}" "${bam_dir}/${name//.gz/}.unsorted.bam" && rm "${bam_dir}/${name//.gz/}.unsorted.bam"
 
 echo -e "\n`date`:[M]: Alignment complete. Removing fastqs for disk and moving alignment file..."
 rm "$r1" "$r2"
@@ -51,11 +51,11 @@ touch ${jd}/work/intdf137/splits${r1//.gz/}
 touch ${jd}/work/intdf137/splits${r2//.gz/}
 mv "${bam_dir}/${out}" ${jd}/work/intdf137/splits/
 
-#echo "`date`:[M]: Beginning juicer chimeric task $SLURM_ARRAY_TASK_ID."
-#cd ${jd}
+echo "`date`:[M]: Beginning juicer chimeric task $SLURM_ARRAY_TASK_ID."
+cd ${jd}
 
-#${jd}/scripts/juicer_justchimeric.sh -f --assembly -g "$gid" -d "${jd}/work/intdf137" -s "$site" -S chimeric \
-#-p references/intdf137.chrom.sizes -y restriction_sites/intdf137_Arima.txt \
-#-z references/interior_primary_final.fa -D "$jd" -t "$threads"
+${jd}/scripts/juicer_justchimeric.sh -f --assembly -g "$gid" -d "${jd}/work/intdf137" -s "$site" -S chimeric \
+-p references/intdf137.chrom.sizes -y restriction_sites/intdf137_Arima.txt \
+-z references/interior_primary_final.fa -D "$jd" -t "$threads"
 
-#echo -e "`date`:[M]: Juicer chimeric processing task $SLURM_ARRAY_TASK_ID complete."
+echo -e "`date`:[M]: Juicer chimeric processing task $SLURM_ARRAY_TASK_ID complete."
