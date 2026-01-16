437c57c4932ebfbf871143658970ccc612c5d1ea
Meg Smith <meg8130@student.ubc.ca>
2026-01-15 18:39:42 -0800

    Update mrna_gff32gtf.py

--- mrna_gff32gtf.py
+++ mrna_gff32gtf.py
@@ -10,9 +10,7 @@ if __name__ == "__main__":
 with open(ingff) as f, open(outgtf, "w") as of:
     for line in f:
         fields=line.strip().split("\t")
-        if fields[2] != "mRNA":
-            continue
-        else:
+        if fields[2] == "mRNA":
             info=fields[8].split(";")
             gene_id=info[1].split("=")[1]
             transcript_id=info[0].split("=")[1]
@@ -20,3 +18,5 @@ with open(ingff) as f, open(outgtf, "w") as of:
             fields[8]=f'gene_id "{gene_id}"; transcript_id "{transcript_id}"'
 
             of.write('\t'.join(map(str,fields)) + '\n')
+        else:
+            continue
