33fe574b3fdb866db2246e18fc68e59e213ac099
Meg Smith <meg8130@student.ubc.ca>
2026-01-22 14:34:56 -0800

    Update split_annotation.py

--- genome_annotation/split_annotation.py
+++ genome_annotation/split_annotation.py
@@ -10,35 +10,39 @@ with open("split_scaffold_lengths.tsv") as f1:
         else:
             continue
 
-with open("gene_coord.tmp") as f2:
+with open("gene_coord.tmp") as f2, open("gene_coord_split.tmp","w") as of:
     for line in f2:
         fields=line.strip().split("\t")
         unbroken_scaff=fields[0]
         chrnum=int(unbroken_scaff.split("_")[2])
 
-        search_for="HiC_scaffold_" + str(chrnum) + "_1"
+        if chrnum < 7:
+            search_for="HiC_scaffold_" + str(chrnum) + "_1"
 
-        chr_fragone_len=int(scaff_idx[search_for])
+            chr_fragone_len=int(scaff_idx[search_for])
 
-        start=int(fields[1])
-        end=int(fields[2])
+            start=int(fields[1])
+            end=int(fields[2])
 
-        if start > end:
-            continue
+            if start > end:
+                continue
 
-        if end <= chr_fragone_len:
-            continue
-            #fields[0]=search_for
-            #print("\t".join(map(str,fields)) + '\n')
-        elif end > chr_fragone_len and start < chr_fragone_len:
-            print(line)
-        elif start > chr_fragone_len:
-            print(fields)
-            fields[0]="HiC_scaffold_" + str(chrnum) + "_2"
-            subt=chr_fragone_len-1
-            fields[1]=start-subt
-            fields[2]=end-subt
-            print(fields)
+            if end <= chr_fragone_len:
+                continue
+                fields[0]=search_for
+                of.write("\t".join(map(str,fields)) + '\n')
+            elif end > chr_fragone_len and start =< chr_fragone_len:
+                continue
+                    #skip records that are on the split
+            elif start > chr_fragone_len:
+                fields[0]="HiC_scaffold_" + str(chrnum) + "_2"
+                subt=chr_fragone_len-1
+                fields[1]=start-subt
+                fields[2]=end-subt
+                of.write("\t".join(map(str,fields) + '\n'))
+            else:
+                print(fields)
+                print("error parsing coordinates")
+
         else:
-            print(fields)
-            print("error parsing coordinates")
+            of.write("\t".join(map(str, fields)) + '\n')
