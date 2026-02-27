598e104a03d08b32884a32aeff6b8211ce970107
Meg Smith <meg8130@student.ubc.ca>
2026-02-27 12:09:52 -0800

    Update run_3DDNA_mantis.sh

--- manual_curation/manual_curation_round2/run_3DDNA_mantis.sh
+++ manual_curation/manual_curation_round2/run_3DDNA_mantis.sh
@@ -3,7 +3,7 @@
 #SBATCH -p general
 #SBATCH -q general
 #SBATCH -c 36
-#SBATCH --mem=500G
+#SBATCH --mem=1000G
 #SBATCH -o %x.%j.out
 #SBATCH -e %x.%j.err
 
@@ -27,9 +27,8 @@ out_vis=${scratch}/3ddna
 prim=${core}/CBP_assemblyfiles/interior_primary_final.fa
 export TMPDIR=${scratch}
 
-cd ${out_vis}
+cd ${out_fulldir}
 merged_nodups=${jd}/work/intdf137/aligned/merged_nodups.txt
-asm_mnd=${scratch}/temp.interior_primary_final.0.asm_mnd.txt
 
 echo -e "\n`date`:[M]: Beginning full 3DDNA pipeline.\n"
-${core}/bin/3d-dna/visualize/run-asm-visualizer.sh -i -m ${asm_mnd} ${out_fulldir}/interior_primary_final.0.cprops ${out_fulldir}/interior_primary_final.0.asm ${merged_nodups}
+${core}/bin/3d-dna/visualize/run-asm-pipeline.sh -f ${prim} ${merged_nodups}
