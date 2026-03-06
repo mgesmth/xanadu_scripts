4ce4e44f841f9d7ab60b23ae2de852aa9ed00514
Meg Smith <meg8130@student.ubc.ca>
2026-03-06 08:32:06 -0800

    Update prep_forjuicer.sh

--- manual_curation/manual_curation_round2_alt/prep_forjuicer.sh
+++ manual_curation/manual_curation_round2_alt/prep_forjuicer.sh
@@ -16,7 +16,7 @@ module load bwa/0.7.17 samtools/1.20 python/3.8.1 seqkit/2.10.0
 home=/home/FCAM/msmith
 core=/core/projects/EBP/smith
 scratch=/scratch/msmith
-juicedir=${scratch}/juicer_formanualcur
+juicedir=${core}/juicer_alt
 export PATH="${juicedir}/scripts:$PATH"
 alt=${juicedir}/references/interior_alternate_final.fa
 alt_name=$(basename ${alt})
@@ -25,13 +25,9 @@ enzyme="Arima"
 
 R1=${home}/hiC_data/allhiC_R1.fastq.gz
 R2=${home}/hiC_data/allhiC_R2.fastq.gz
-hic_split=${scratch}/hic_split
-hic_bams=${scratch}/hic_bams
+hic_split=${juicedir}/work/intdf_137/fastq
+hic_bams=${juicedir}/work/intdf_137/splits
 splitN=300
-
-if [[ ! -d "$hic_split" ]] ; then
-  mkdir ${hic_split}
-fi
 
 echo -e "\n[M]: Splitting Hi-C data\n"
 
