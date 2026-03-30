f81573ec4a87800d116f84c5528ad4c0e1fdb992
Meg Smith <meg8130@student.ubc.ca>
2026-03-30 12:00:25 -0700

    Update mitohifi_test.sh

--- mitohifi_test.sh
+++ mitohifi_test.sh
@@ -12,7 +12,7 @@ core=/core/projects/EBP/smith
 asm=${home}/dummy.fasta
 outdir=${home}/mitohifi/reads
 
-module load MitoHiFi/3.2.1
+module load MitoHiFi/3.2.1 python/3.8.1
 
 #findMitoReference.py --species "Pseudotsuga menziesii" --outfolder ${outdir}
 #Downloaded Lacebark pine mitogenome
@@ -23,4 +23,4 @@ echo -e "`date`:[M]: Beginning mitogenome assembly from scaffolded assembly.\n"
 
 cd ${outdir_c}
 
-MitoHiFi -c ${asm} -f "${relative}.fasta" -g "${relative}.gbk" -t 4 -a "plant" 
+python mitohifi.py -c ${asm} -f "${relative}.fasta" -g "${relative}.gbk" -t 4 -a "plant" 
