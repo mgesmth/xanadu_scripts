7e23f7ffc6768571d11a6379ab5f6ec07ac86699
Meg Smith <meg8130@student.ubc.ca>
2026-04-16 13:33:20 -0700

    Update 02_bwa_alignments_redo.sh

--- repadapt_uc/02_bwa_alignments_redo.sh
+++ repadapt_uc/02_bwa_alignments_redo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH -J 02.BWA
-#SBATCH -o 98_log_files/%x_%j.out
-#SBATCH -e 98_log_files/%x_%j.err
+#SBATCH -o 98_log_files/%x_%A_%a.out
+#SBATCH -e 98_log_files/%x_%A_%a.err
 #SBATCH -c 8
 #SBATCH --mem=48G
 
@@ -16,7 +16,6 @@ TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 SCRIPT=$0
 NAME=$(basename $0)
 LOG_FOLDER="98_log_files"
-cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"_%J
 
 # Global variables
 GENOMEFOLDER="03_genome"
@@ -34,8 +33,8 @@ fi
 
 # If this is our first run, make a list of all the trimmed reads for cleaning
 
-array=($(cat $RAWDATAFOLDER/all_trimmed_ids.txt))
-name=${array[0]}
+array=($(cat $RAWDATAFOLDER/redo_files.txt))
+name=${array[$SLURM_ARRAY_TASK_ID]}
 
     # Name of uncompressed file
     file1=${name}.R1.trimmed.fastq.gz
