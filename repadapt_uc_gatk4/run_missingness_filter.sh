6dc12c124e59a4316261ba7e123937ac0e700735
Meg Smith <meg8130@student.ubc.ca>
2026-04-26 21:50:46 -0400

    commit

---
+++ repadapt_uc_gatk4/run_missingness_filter.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -J filter
+#SBATCH -D /core/projects/EBP/smith/linkage_snp_calling/10b_filt_vcfs
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 12
+#SBATCH --mem=36G
+#SBATCH -o %x.%j.out
+#SBATCH -e %x.%j.err
+
+echo "[M]: Host Name: `hostname`"
+set -e
+
+module load python/3.13.11-gcc-11.4.0-kifh66l tabix/0.2.6
+
+bgzip -d linkage_snp_calling_gatk_filtered_pass_biallelic_indels.vcf.gz
+
+in_vcf=linkage_snp_calling_gatk_filtered_pass_biallelic_indels.vcf
+out_vcf=linkage_snp_calling_gatk_filtered_pass_biallelic_indels_missingness.vcf
+
+python3 /home/FCAM/msmith/scripts/repadapt_uc_gatk4/filter_GQ_missingness_linkagesnps.py \
+0.5 "$in_vcf" "$out_vcf"
+
+bgzip "$out_vcf"
+tabix -p vcf "${out_vcf}.gz"
