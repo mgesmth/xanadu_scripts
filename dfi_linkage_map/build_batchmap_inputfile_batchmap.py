33b14787243ff16bcd31364773034c5d5f02a0a4
Meg Smith <meg8130@student.ubc.ca>
2026-05-01 20:18:05 -0700

    Update build_batchmap_inputfile_batchmap.py

--- dfi_linkage_map/build_batchmap_inputfile_batchmap.py
+++ dfi_linkage_map/build_batchmap_inputfile_batchmap.py
@@ -55,8 +55,8 @@ with open(vcf) as f, open(raw_tmp, "w") as of:
 
             #ANDDDD put it all together
             marker_name="*" + fields[0] + "_" + fields[1]
-            segs=",".join(map(str,seg_genos))
-            newline=[marker_name,marker_type].append(segs)
+            segs=[",".join(map(str,seg_genos))]
+            newline=[marker_name,marker_type] + segs
             passed_counter+=1
             of.write(" ".join(map(str,newline)) + '\n')
 
