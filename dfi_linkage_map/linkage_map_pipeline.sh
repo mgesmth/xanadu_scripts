c1321203e4aa3efca5778ca4f03ea1a7d9c8a42d
Meg Smith <meg8130@student.ubc.ca>
2026-05-03 22:29:10 -0700

    Update linkage_map_pipeline.sh

--- dfi_linkage_map/linkage_map_pipeline.sh
+++ dfi_linkage_map/linkage_map_pipeline.sh
@@ -22,53 +22,52 @@ binned_raw=DFI_linkage_stringent_maf_binned.raw
 mark2=DFI_linkage_stringent_maf_binned_segpass.txt
 num_samp=100
 
-'''
-singularity exec ${batchmap} \
-Rscript ../01_scripts/batchmap_segdist.R "$mark1" "$binned_raw"
-transform onemap raw file to batchmap txt file
-python ../01_scripts/onemap_raw_to_batchmap_txt.py "$binned_raw" tmp.txt
 
-remove segregation distorters from map
-awk 'NR==FNR{
-  if ($1 == "marker") {
-    next
-  } else {
-    passed_arr[$1]=1
-  }
-  next
-}{
-  if ($0 ~ "scaffold") {
-    marker=substr($1,2,length($1))
-    if (marker in passed_arr) {
-      print
-    } else {
-      next
-    }
-  } else {
-    next
-  }
-}' seg_passed_markers.tsv tmp.txt > tmp1.txt
+#singularity exec ${batchmap} \
+#Rscript ../01_scripts/batchmap_segdist.R "$mark1" "$binned_raw"
+#transform onemap raw file to batchmap txt file
+#python ../01_scripts/onemap_raw_to_batchmap_txt.py "$binned_raw" tmp.txt
 
-num_marks=$(cat tmp1.txt | wc -l)
-echo "${num_samp} ${num_marks} 0" > ${mark2}
-cat tmp1.txt >> ${mark2}
-rm tmp.txt tmp1.txt
-'''
+#remove segregation distorters from map
+#awk 'NR==FNR{
+#  if ($1 == "marker") {
+#    next
+#  } else {
+#    passed_arr[$1]=1
+#  }
+#  next
+#}{
+#  if ($0 ~ "scaffold") {
+#    marker=substr($1,2,length($1))
+#    if (marker in passed_arr) {
+#      print
+#    } else {
+#      next
+#    }
+#  } else {
+#    next
+#  }
+#}' seg_passed_markers.tsv tmp.txt > tmp1.txt
+
+#num_marks=$(cat tmp1.txt | wc -l)
+#echo "${num_samp} ${num_marks} 0" > ${mark2}
+#cat tmp1.txt >> ${mark2}
+#rm tmp.txt tmp1.txt
+
 
 ###
 #Now create linkage groups
 singularity exec ${batchmap} RScript \
 ../01_scripts/batchmap_createLGs.R ${dir} ${mark2}
 
-'''
+
 #Create maps for each LG in parallel
 
-num_LGs=13
-array=$((${num_LGs2}-1))
+#num_LGs=13
+#array=$((${num_LGs2}-1))
 
-sbatch -J LG_batchmap -p general -q general \
---array=[0-${array}] \
--c 24 --mem=128G --mail-user=meg8130@student.ubc.ca \
---mail-type=ALL -o %x.%A.%a.out -e %x.%A.%a.err \
-../01_scripts/parallel_batchmaps.sh
-'''
+#sbatch -J LG_batchmap -p general -q general \
+#--array=[0-${array}] \
+#-c 24 --mem=128G --mail-user=meg8130@student.ubc.ca \
+#--mail-type=ALL -o %x.%A.%a.out -e %x.%A.%a.err \
+#../01_scripts/parallel_batchmaps.sh
