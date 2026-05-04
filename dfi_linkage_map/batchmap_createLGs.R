f18e69e2a81b4358f4023245a1a055492ce18927
Meg Smith <meg8130@student.ubc.ca>
2026-05-03 22:14:08 -0700

    Update batchmap_createLGs.R

--- dfi_linkage_map/batchmap_createLGs.R
+++ dfi_linkage_map/batchmap_createLGs.R
@@ -11,8 +11,22 @@ outcross <- read.outcross2(input_file)
 #estimate two-point r
 twopt_table <- rf.2pts(outcross)
 #find linkage groups
+#suggest_lod from onemap; batchmap function doesn't work
+suggest_lod <- function(x) {
+  if (inherits(x, c("sequence", "onemap","outcross"))) {
+    if (inherits(x, c("onemap","outcross")))
+      num.tests <- choose(x$n.mar, 2)
+    if (inherits(x, "sequence"))
+      num.tests <- choose(length(x$seq.num), 2)
+    LOD <- 0.2172 * qchisq(1 - 0.05/num.tests, 1)
+    return(LOD)
+  }
+  else stop("This is not a onemap object with raw data")
+}
+LOD=suggest_lod(outcross)
+
 linkage_groups <- group(make.seq(input.obj = twopt_table, "all"),
-                        LOD = 12)
+                        LOD = LOD, max.rf = 0.35)
 print(linkage_groups,detailed=F)
 
 LG_list <- list()
