f52f386edbecbf3ba7a965819d4e5605824a280e
Meg Smith <meg8130@student.ubc.ca>
2026-05-07 15:38:23 -0700

    commit

--- dfi_linkage_map/run_missingness_filter.sh
+++ dfi_linkage_map/run_missingness_filter.sh
@@ -17,7 +17,7 @@ source /home/FCAM/msmith/python_venv/bin/activate
 #bgzip -d linkage_snp_calling_gatk_filtered_pass_biallelic_indels.vcf.gz
 
 in_vcf=linkage_snp_calling_gatk_filtered_pass_biallelic_indels.vcf
-out_vcf=linkage_snp_calling_gatk_filtered_pass_biallelic_indels_missingness.vcf
+out_vcf_prx=linkage_snp_calling_gatk_filtered_pass_biallelic_indels_missingness
 
 python3 /home/FCAM/msmith/scripts/dfi_linkage_map/filter_GQ_missingness_maternal.py \
 0.3 0.5 30 "$in_vcf" "$out_vcf"
