d32e2faf1eb4364cec6b1e188cc33e4afcb8fd17
Meg Smith <meg8130@student.ubc.ca>
2026-05-07 10:41:07 -0700

    Update batchmap_createLGs.R

--- dfi_linkage_map/batchmap/batchmap_createLGs.R
+++ dfi_linkage_map/batchmap/batchmap_createLGs.R
@@ -3,8 +3,8 @@ library(BatchMap)
 args <- commandArgs(trailingOnly = TRUE)
 wd=args[1]
 input_file=args[2]
-max_rf=args[3]
-LOD=args[4]
+max_rf=as.double(args[3])
+LOD=as.double(args[4])
 
 setwd(wd)
 
@@ -15,11 +15,11 @@ bins <- find.bins(outcross, exact = FALSE)
 outcross_clean <- create.data.bins(outcross, bins)
 
 #estimate two-point r
-twopt_table <- rf.2pts(outcross_clean,LOD=LOD,max.rf=0.35)
+twopt_table <- rf.2pts(outcross_clean,LOD=LOD,max.rf=max_rf)
 
 #find linkage groups
 linkage_groups <- group(make.seq(input.obj = twopt_table, "all"),
-                        LOD = LOD, max.rf = 0.35)
+                        LOD = LOD, max.rf = max_rf)
 print(linkage_groups,detailed=F)
 
 LG_list <- list()
