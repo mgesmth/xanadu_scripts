d046172c415af036382829281412ab3dad7bc398
Meg Smith <meg8130@student.ubc.ca>
2026-05-15 06:36:09 -0700

    Update linkage_map_pipeline.sh

--- dfi_linkage_map/batchmap/linkage_map_pipeline.sh
+++ dfi_linkage_map/batchmap/linkage_map_pipeline.sh
@@ -17,52 +17,54 @@ module load python/3.13.11-gcc-11.4.0-kifh66l
 
 core=/core/projects/EBP/smith
 batchmap=${core}/bin/batchmap.sif
-dir=${core}/linkage_snp_calling/11_batchmap
+dir=${core}/linkage_snp_calling_unsplit/11b_batchmap
 scripts=${dir}/scripts
 if [[ ! -d ${scripts} ]] ; then
   ln -s /home/FCAM/msmith/scripts/dfi_linkage_map/batchmap ./scripts
 fi
 
 cd ${dir}
-mark1=DFI_linkage_stringent_maf.txt
-mark2=DFI_linkage_stringent_maf_segpass.txt
+mark1=$1
+#linkage_snp_calling_unsplit_batchmap_standard.txt
+mark2=$2
+#linkage_snp_calling_unsplit_batchmap_standard_segpass.txt
 num_samp=100
-max_rf=$1
-LOD=$2
+max_rf=$3
+LOD=$4
 
-#echo -e "\n[M]: Finding segregation distorters...\n"
-#cp ${scripts}/batchmap_segdist.R .
-#singularity exec ${batchmap} Rscript batchmap_segdist.R ${dir} ${mark1}
-#rm batchmap_segdist.R
+echo -e "\n[M]: Finding segregation distorters...\n"
+cp ${scripts}/batchmap_segdist.R .
+singularity exec ${batchmap} Rscript batchmap_segdist.R ${dir} ${mark1}
+rm batchmap_segdist.R
 
-#echo -e "\n[M]: Removing segregation distorters...\n"
+echo -e "\n[M]: Removing segregation distorters...\n"
 
 #remove segregation distorters
-#awk 'NR==FNR{
-#  if ($1 == "marker") {
-#    next
-#  } else {
-#    passed_arr[$1]=1
-#  }
-#  next
-#}{
+awk 'NR==FNR{
+  if ($1 == "marker") {
+    next
+  } else {
+    passed_arr[$1]=1
+  }
+  next
+}{
   #if the line is a marker line and not the header
-#  if ($0 ~ "scaffold") {
-#    mark=substr($1,2,length($1))
-#    if (mark in passed_arr) {
-#      print
-#    } else {
-#      next
-#    }
-#  } else {
+  if ($0 ~ "scaffold") {
+    mark=substr($1,2,length($1))
+    if (mark in passed_arr) {
+      print
+    } else {
+      next
+    }
+  } else {
     #if the header line
-#    next
-#  }
-#}' seg_passed_markers_notbinned.tsv ${mark1} > marks.tmp
+    next
+  }
+}' seg_passed_markers_notbinned.tsv ${mark1} > marks.tmp
 
-#num_marks=$(cat marks.tmp | wc -l)
-#echo "100 ${num_marks} 0" > ${mark2}
-#cat marks.tmp >> ${mark2} && rm marks.tmp
+num_marks=$(cat marks.tmp | wc -l)
+echo "100 ${num_marks} 0" > ${mark2}
+cat marks.tmp >> ${mark2} && rm marks.tmp
 
 echo -e "\n[M]: Finding linkage groups..."
 
