32470fcbdc68898acf749bd3ed4c7308354b7473
Meg Smith <meg8130@student.ubc.ca>
2026-06-12 15:15:14 -0700

    Create estimate_ploidy_of_megagametophytes.sh

---
+++ dfi_linkage_map/estimate_ploidy_of_megagametophytes.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH -J kmer_ploidy
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -D /core/projects/EBP/smith/linkage_snp_calling_unsplit/ploidy_estimation
+#SBATCH -c 12
+#SBATCH --mem=36G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=meg8130@student.ubc.ca
+#SBATCH -o /core/projects/EBP/smith/linkage_snp_calling_unsplit/ploidy_estimation/log/%x.%j.out
+#SBATCH -e /core/projects/EBP/smith/linkage_snp_calling_unsplit/ploidy_estimation/log/%x.%j.err
+
+echo "[M]: Host Name: `hostname`"
+set -e
+
+core=/core/projects/EBP/smith
+topdir=${core}/linkage_snp_calling_unsplit
+dir=${topdir}/ploidy_estimation
+trimdir=${topdir}/05_trimmed_data
+
+module load meryl/1.4.1 module load R/4.2.2
+export PATH="/home/FCAM/msmith/R/x86_64-pc-linux-gnu-library/4.2:$PATH"
+export PATH="/core/projects/EBP/smith/bin/genomescope2.0:$PATH"
+
+#R1s.txt is a list of the trimmed R1 fastqs (no dir structure)
+
+for file in $(cat R1s.txt); do
+	name=${file%.R1.trimmed.fastq.gz}
+	file_r2=$(echo "$file" | sed 's/.R1/.R2/')
+	if [[ ! -d "$name" ]] ; then
+		mkdir $name
+	fi
+	cd $name 
+	echo "[M]: Counting ${name} kmers..."
+	meryl count threads=12 k=21 ${trimdir}/${file} output ${name}.R1.meryl
+	meryl count threads=12 k=21 ${trimdir}/${file_r2} output ${name}.R2.meryl
+	meryl union-sum ${name}.R1.meryl ${name}.R2.meryl output ${name}.meryl
+    echo "[M]: Done counting kmers. Getting histogram..."
+    meryl histogram threads=12 k=21 ${name}.meryl > ${name}.meryl.hist 
+    echo "[M]: Running GenomeScope2..."
+    genomescope.R -i ${name}.meryl.hist -o . -k 21
+    echo "[M]: Done with ${name}."
+fi
+
+	
+
+
