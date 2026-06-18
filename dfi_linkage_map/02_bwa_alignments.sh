c96441439a74a4c99e6e4f12b697a5370c71efe4
Meg Smith <meg8130@student.ubc.ca>
2026-06-18 15:56:18 -0700

    Update 02_bwa_alignments.sh

--- dfi_linkage_map/02_bwa_alignments.sh
+++ dfi_linkage_map/02_bwa_alignments.sh
@@ -18,6 +18,7 @@ NAME=$(basename $0)
 LOG_FOLDER="98_log_files"
 
 # Global variables
+INFO="02_info_files"
 GENOMEFOLDER="03_genome"
 GENOME=$(ls -1 $GENOMEFOLDER/*{fasta,fa,fasta.gz,fa.gz} | xargs -n 1 basename)
 INDGENOME=${GENOME}.fai
@@ -31,38 +32,12 @@ then
     NCPU=4
 fi
 
-# If this is our first run, make a list of all the trimmed reads for cleaning
-ls -1 $RAWDATAFOLDER/*R1.trimmed.fastq.gz | xargs -n 1 basename | sed 's/.R1.trimmed.fastq.gz//g' > $RAWDATAFOLDER/all_trimmed_ids.txt
-
-array=($(cat $RAWDATAFOLDER/all_trimmed_ids.txt))
+array=($(cat $INFO/datatable.txt | cut -f1))
 name=${array[$SLURM_ARRAY_TASK_ID]}
 
-    # Name of uncompressed file
     file1=${name}.R1.trimmed.fastq.gz
     file2=${name}.R2.trimmed.fastq.gz
-    echo ">>> Aligning file $file1 $file2 <<<
-        "
-
-    # Now clean if we have to
-    # First check whether we need to edit the header
-  if [ $(zcat $RAWDATAFOLDER/$file1 | head -n100 | grep "@" | cut -d" " -f1 | sed 's/\./\t/g' |  awk '{print $NF}' | sort | uniq | wc -l) -eq 1 ];
-  then
-
-    echo ">>> R1 and R2 files for $name have malformed headers. Cleaning and removing suffixes.
-    "
-
-    mv $RAWDATAFOLDER/$file1 $RAWDATAFOLDER/${name}.R1.trimmed_dirty.fastq.gz
-    mv $RAWDATAFOLDER/$file2 $RAWDATAFOLDER/${name}.R2.trimmed_dirty.fastq.gz
-    zcat $RAWDATAFOLDER/${name}.R1.trimmed_dirty.fastq.gz | sed -E "s/^((@|\+)SRR[^.]+\.[^.]+)\.(1|2)/\1/" | gzip > $RAWDATAFOLDER/$file1
-    zcat $RAWDATAFOLDER/${name}.R2.trimmed_dirty.fastq.gz | sed -E "s/^((@|\+)SRR[^.]+\.[^.]+)\.(1|2)/\1/" | gzip > $RAWDATAFOLDER/$file2
-
-    # Keep clean
-    rm -f $RAWDATAFOLDER/${name}.R1.trimmed_dirty.fastq.gz $RAWDATAFOLDER/${name}.R2.trimmed_dirty.fastq.gz
-
-  else
-    echo ">>> R1 and R2 files for $name passed QC
-    "
-  fi
+    echo ">>> Aligning file $file1 $file2 <<<"
 
     # Set ID
     ID="@RG\tID:ind\tSM:ind\tPL:Illumina"
