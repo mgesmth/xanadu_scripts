ed4fe88e68de97cd067d634d2c2d6c95b932d5fd
Meg Smith <meg8130@student.ubc.ca>
2026-06-22 22:51:41 -0700

    commit

--- dfi_linkage_map/batchmap/run_subsample_iteration.sh
+++ dfi_linkage_map/batchmap/run_subsample_iteration.sh
@@ -11,15 +11,16 @@ echo `hostname`
 set -e
 module load singularity/3.9.2
 
-subsample_script=$1
-LG=$2
-iter=$3
+outdir=$1
+subsample_script=$2
+LG=$3
+iter=$4
 batchmap=/core/projects/EBP/smith/bin/batchmap.sif 
 cores=$SLURM_CPUS_PER_TASK
-outdir=$SLURM_SUBMIT_DIR
 
 run=$(basename {subsample_script})
 if [[ ! -f "$run" ]] ; then
 	cp $subsample_script .
 fi
-singularity exec $batchmap Rscript $run $outdir $cores $LG $iter 
+singularity exec $batchmap Rscript $run \
+$cores $outdir $LG $iter 
---
+++ dfi_linkage_map/batchmap/run_subsample_launch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -J subsample_launch
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 1
+#SBATCH --mem=1G
+#SBATCH -D /core/projects/EBP/smith/linkage_actually/11_batchmap_10kb
+#SBATCH -o log/%x.%j.out
+#SBATCH -e log/%x.%j.err
+
+echo `hostname`
+set -e
+
+LG=$1
+outdir="${LG}_subsamples"
+if [[ ! -d "$outdir" ]] ;; then
+	mkdir $outdir
+fi
+subsamp_script=$(pwd)/scripts/batchmap_createsubsampled_maps.R
+
+for i in $(seq 1 10); do
+	sbatch scripts/run_subsample_iteration.sh \
+	${outdir} $subsamp_script $LG $i
+	echo "iteration $i submitted"
+done
+
